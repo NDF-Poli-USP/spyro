ee80fb7dc9c9c3039ab1e8809364fc774a4a1d9d
Keith Roberts <krober10@nd.edu>
2020-12-04 19:19:50 -0300

    adding level set codes in correct place

---
+++ spyro/solvers/advect.py
@@ -0,0 +1,88 @@
+from firedrake import *
+
+
+def advect(mesh, q, theta, number_of_timesteps=10):
+    """Advect a mesh with two subdomains based on the shape gradient `theta`"""
+
+    V = FunctionSpace(mesh, "DG", 0)
+    W = VectorFunctionSpace(mesh, "CG", 1)
+
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    # q = Function(V)
+    # make the assumption that subdomains are named 10 and 11
+    # solve(u * v * dx == 1 * v * dx(10) + -1 * v * dx(11), q)
+
+    u = Function(W).assign(theta)
+
+    outfile = File("DGadv_domain.pvd")
+    outfile.write(q)
+
+    dt = 0.0001
+    T = dt * number_of_timesteps
+    dtc = Constant(dt)
+    q_in = Constant(1.0)
+
+    dq_trial = TrialFunction(V)
+    phi = TestFunction(V)
+    a = phi * dq_trial * dx
+
+    n = FacetNormal(mesh)
+    un = 0.5 * (dot(u, n) + abs(dot(u, n)))
+
+    L1 = dtc * (
+        q * div(phi * u) * dx
+        - conditional(dot(u, n) < 0, phi * dot(u, n) * q_in, 0.0) * ds
+        - conditional(dot(u, n) > 0, phi * dot(u, n) * q, 0.0) * ds
+        - (phi("+") - phi("-")) * (un("+") * q("+") - un("-") * q("-")) * dS
+    )
+
+    q1 = Function(V)
+    q2 = Function(V)
+    L2 = replace(L1, {q: q1})
+    L3 = replace(L1, {q: q2})
+
+    dq = Function(V)
+
+    params = {"ksp_type": "preonly", "pc_type": "bjacobi", "sub_pc_type": "ilu"}
+    prob1 = LinearVariationalProblem(a, L1, dq)
+    solv1 = LinearVariationalSolver(prob1, solver_parameters=params)
+    prob2 = LinearVariationalProblem(a, L2, dq)
+    solv2 = LinearVariationalSolver(prob2, solver_parameters=params)
+    prob3 = LinearVariationalProblem(a, L3, dq)
+    solv3 = LinearVariationalSolver(prob3, solver_parameters=params)
+
+    t = 0.0
+    step = 0
+    while t < T - 0.5 * dt:
+
+        solv1.solve()
+        q1.assign(q + dq)
+
+        solv2.solve()
+        q2.assign(0.75 * q + 0.25 * (q1 + dq))
+
+        solv3.solve()
+        q.assign((1.0 / 3.0) * q + (2.0 / 3.0) * (q2 + dq))
+
+        if step % 5 == 0:
+            print(t)
+            outfile.write(q)
+        step += 1
+        t += dt
+
+    # relabel the cells
+    sd10 = SubDomainData(q > 0)
+    sd11 = SubDomainData(q < 0)
+
+    # solve for the new q
+    uu = TrialFunction(V)
+    vv = TestFunction(V)
+
+    # reset q
+    q.assign(1)
+    q.interpolate(Constant(1), sd1)
+    q.interpolate(Constant(-1), sd2)
+    File("final_q.pvd").write(q)
+
+    return q, [sd10, sd11]
