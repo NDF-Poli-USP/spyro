c33c64aa7dc8032cff59ef8df08ddad32c69f863
Keith Roberts <krober@usp.edu>
2021-03-08 17:55:44 -0300

    use Gerschgorin's theorem

--- spyro/utils/estimate_timestep.py
+++ spyro/utils/estimate_timestep.py
@@ -1,14 +1,19 @@
 import scipy
 import numpy as np
-from scipy.sparse.linalg import inv
 
 import firedrake as fd
 from firedrake import dot, grad
 import finat
 
 
-def estimate_timestep(mesh, V, c):
-    """Estimate the maximum stable timestep based on the spectral radius"""
+def estimate_timestep(mesh, V, c, estimate_max_eigenvalue=True):
+    """Estimate the maximum stable timestep based on the spectral radius
+    using optionally the Gershgorin Circle Theorem to estimate the
+    maximum generalized eigenvalue. Otherwise computes the maximum
+    generalized eigenvalue exactly
+
+    ONLY WORKS WITH KMV ELEMENTS
+    """
 
     u, v = fd.TrialFunction(V), fd.TestFunction(V)
     quad_rule = finat.quadrature.make_quadrature(
@@ -17,8 +22,14 @@ def estimate_timestep(mesh, V, c):
     dxlump = fd.dx(rule=quad_rule)
     A = fd.assemble(u * v * dxlump)
     ai, aj, av = A.petscmat.getValuesCSR()
+    av_inv = []
+    for value in av:
+        if value == 0:
+            av_inv.append(0.0)
+        else:
+            av_inv.append(1 / value)
     Asp = scipy.sparse.csr_matrix((av, aj, ai))
-    Asp_inv = inv(Asp)
+    Asp_inv = scipy.sparse.csr_matrix((av_inv, aj, ai))
 
     K = fd.assemble(c * c * dot(grad(u), grad(v)) * dxlump)
     ai, aj, av = K.petscmat.getValuesCSR()
@@ -26,13 +37,17 @@ def estimate_timestep(mesh, V, c):
 
     # operator
     Lsp = Asp_inv.multiply(Ksp)
+    if estimate_max_eigenvalue:
+        # absolute maximum of diagonals
+        max_eigval = np.amax(np.abs(Lsp.diagonal()))
+    else:
+        max_eigval = scipy.sparse.linalg.eigs(
+            Ksp, M=Asp, k=1, which="LM", return_eigenvectors=False
+        )[0]
 
-    max_eigval = scipy.sparse.linalg.eigs(
-        Lsp, k=1, which="LM", return_eigenvectors=False
-    )
     # print(max_eigval)
-    max_dt = np.float(2 / np.sqrt(max_eigval[0]))
+    max_dt = np.float(2 / np.sqrt(max_eigval))
     print(
-        f"Maximum stable timestep should be about: {np.float(2 / np.sqrt(max_eigval[0]))} seconds"
+        f"Maximum stable timestep should be about: {np.float(2 / np.sqrt(max_eigval))} seconds"
     )
     return max_dt
