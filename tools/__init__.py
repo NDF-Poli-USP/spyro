d4625301084cd2baad904e78c9e60f8f72cb16b5
Alexandre Olender <olender@usp.br>
2021-03-10 09:00:55 -0300

    Adding tools function calls

---
+++ tools/__init__.py
@@ -0,0 +1,9 @@
+from .grid_point_calculator import wave_solver, generate_mesh, error_calc
+from .input_models import create_model_for_grid_point_calculation
+
+__all__ = [
+    "wave_solver",
+    "generate_mesh",
+    'error_calc'
+    'create_model_for_grid_point_calculation'
+]
