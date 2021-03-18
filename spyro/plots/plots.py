b543e6e1c9c800f61163abfaa5070ceed0e1a720
Alexandre Olender <olender@usp.br>
2021-03-18 12:09:55 -0300

    adding plot for receiver comparison

--- spyro/plots/plots.py
+++ spyro/plots/plots.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 
 import numpy as np
+import spyro
 
 __all__ = ["plot_shotrecords", "plot_shotrecords_difference"]
 
@@ -184,6 +185,73 @@ def plot_pml_error(model, error, appear=False):
     plt.subplots_adjust(left=0.18, right=0.95, bottom=0.14, top=0.95)
     plt.legend(loc="best")
     plt.savefig("pml_error.pdf", format="PDF")
+    if appear:
+        plt.show()
+    plt.close()
+    return None
+
+def plot_receiver_difference(model, p_receiver0, p_receiver1, appear = False, name = 'Receivers', ft = 'PDF'):
+    """Plots two receivers in time
+
+    Parameters
+    ----------
+    model: `dictionary`
+        Contains model parameters and options.
+    p_receiver0: 
+        
+    p_receiver1: 
+        
+    appear: `boolean`, optional
+        Should the images appear on screen?
+    name: string, optional
+        The name of the saved PDF
+    vmax: float, optional
+        The maximum value to plot on the colorscale
+    ft: string, optional
+        File format, PDF or png
+
+    Returns
+    -------
+    None
+
+    """
+
+    final_time = model["timeaxis"]["tf"]
+    # Check if shapes are matching
+    times0, receivers0 = p_receiver0.shape
+    times1, receivers1 = p_receiver1.shape
+
+    if times0 == times1:
+        dt = model["timeaxis"]["dt"]
+
+    else:
+        dt = final_time/times0
+        p_receiver1 = spyro.tools.grid_point_calculator.time_interpolation(p_receiver1, p_receiver0, model)
+
+    nt = int(final_time / dt)  # number of timesteps
+
+    x_rec = np.linspace(start_index, end_index, num_recvs)
+    t_rec = np.linspace(0.0, tf, nt)
+    X, Y = np.meshgrid(x_rec, t_rec)
+
+    time_vector = np.linspace(0.0, final_time, dt)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.tick_params(reset=True, direction="in", which="both")
+    plt.rc("legend", **{"fontsize": 18})
+    plt.plot(time_vector, p_receiver0, 'bo', time_vector, p_receiver1, 'go' ) 
+    ax.yaxis.get_offset_text().set_fontsize(18)
+    plt.subplots_adjust(left=0.18, right=0.95, bottom=0.14, top=0.95)
+    plt.legend(loc="best")
+    plt.xlabel("time (s)", fontsize=18)
+    plt.ylabel("value ", fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlim(0.0, final_time)
+    #plt.ylim(tf, 0)
+    plt.savefig("Receivers." + ft, format=ft)
+    # plt.axis("image")
     if appear:
         plt.show()
     plt.close()
