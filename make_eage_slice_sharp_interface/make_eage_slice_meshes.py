faa5eb226bcabb40be6c1f70ab5a3b70d8d31573
Keith Roberts <krober@usp.br>
2021-03-20 22:58:33 -0300

    Update make_eage_slice_meshes.py

--- make_eage_slice_sharp_interface/make_eage_slice_meshes.py
+++ make_eage_slice_sharp_interface/make_eage_slice_meshes.py
@@ -46,45 +46,45 @@ meshio.write_points_cells(
 )
 
 
-#fname = "eage_slice_guess.segy"
-## Bounding box describing domain extents (corner coordinates)
-## top right corner is at (0.0, 13520.0, 13520.0)
-#bbox = (-4200.0, 0.0, 0.0, 13520.0)
-#
-## Desired minimum mesh size in domain
-#wl = 10
-#freq = 5
-#hmin = 1500.0 / (wl * freq)
-#
-#rectangle = sm.Rectangle(bbox)
-#
-## Construct mesh sizing object from velocity model
-#ef = sm.get_sizing_function_from_segy(
-#    fname,
-#    bbox,
-#    hmin=hmin,
-#    wl=wl,
-#    freq=freq,
-#    dt=0.001,
-#    grade=0.15,
-#    domain_pad=500,
-#    pad_style="edge",
-#)
-#
-#points, cells = sm.generate_mesh(domain=rectangle, edge_length=ef)
-#
-#sm.write_velocity_model(
-#    fname,
-#    ofname="eage_guess_slice",
-#    bbox=bbox,
-#    domain_pad=500.0,
-#    pad_style="edge",
-#    units="m-s",
-#)
-#meshio.write_points_cells(
-#    "eage_guess_slice.msh",
-#    points / 1000.0,
-#    [("triangle", cells)],
-#    file_format="gmsh22",
-#    binary=False,
-#)
+fname = "eage_slice_guess.segy"
+# Bounding box describing domain extents (corner coordinates)
+# top right corner is at (0.0, 13520.0, 13520.0)
+bbox = (-4200.0, 0.0, 0.0, 13520.0)
+
+# Desired minimum mesh size in domain
+wl = 10
+freq = 5
+hmin = 1500.0 / (wl * freq)
+
+rectangle = sm.Rectangle(bbox)
+
+# Construct mesh sizing object from velocity model
+ef = sm.get_sizing_function_from_segy(
+    fname,
+    bbox,
+    hmin=hmin,
+    wl=wl,
+    freq=freq,
+    dt=0.001,
+    grade=0.15,
+    domain_pad=500,
+    pad_style="edge",
+)
+
+points, cells = sm.generate_mesh(domain=rectangle, edge_length=ef)
+
+sm.write_velocity_model(
+    fname,
+    ofname="eage_guess_slice",
+    bbox=bbox,
+    domain_pad=500.0,
+    pad_style="edge",
+    units="m-s",
+)
+meshio.write_points_cells(
+    "eage_guess_slice.msh",
+    points / 1000.0,
+    [("triangle", cells)],
+    file_format="gmsh22",
+    binary=False,
+)
