7d54060cf5d3661ddb51d358e3504526cfbb7530
Alexandre Olender <olender@usp.br>
2021-03-24 10:16:05 -0300

    Changing sources to 500

--- spyro/sources/Sources.py
+++ spyro/sources/Sources.py
@@ -149,7 +149,7 @@ def FullRickerWavelet(dt, tf, freq, amp=1.0, cutoff=None):
     return FullWavelet
 
 
-def delta_expr(x0, z, x, sigma_x=1000.0):
+def delta_expr(x0, z, x, sigma_x=500.0):
     sigma_x = Constant(sigma_x)
     return exp(-sigma_x * ((z - x0[0]) ** 2 + (x - x0[1]) ** 2))
 
