6bc7705969b39a3eaf07d36be7024998a434fe2f
Alexandre Olender <olender@usp.br>
2021-04-06 19:37:06 -0300

    copied ssprk33 to 10_4

--- spyro/solvers/__init__.py
+++ spyro/solvers/__init__.py
@@ -1,9 +1,11 @@
 from .leapfrog import Leapfrog
 from .leapfrog_adjoint import Leapfrog_adjoint
 from .ssprk33 import SSPRK33
+from .ssprk104 import SSPRK104
 
 __all__ = [
     "Leapfrog",
     "Leapfrog_adjoint",
     "SSPRK33",
+    "SSPRK104"
 ]
