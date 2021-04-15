bce831a5992cca2a5dd21c497d1bbbd43ecc6741
Alexandre Olender <olender@usp.br>
2021-04-15 16:41:34 -0300

    testing

--- spyro/solvers/__init__.py
+++ spyro/solvers/__init__.py
@@ -1,9 +1,11 @@
 from .leapfrog import Leapfrog
 from .leapfrog_adjoint import Leapfrog_adjoint
 from .ssprk3 import SSPRK3
+from .newssprk import SSPRKMOD
 
 __all__ = [
     "Leapfrog",
     "Leapfrog_adjoint",
     "SSPRK3",
+    "SSPRKMOD"
 ]
