7010c554a59ace89de9632d8b719e83c0227fe3e
Ig-dolci <daia.dolci@gmail.com>
2021-04-28 20:33:33 -0300

    latest version adding the modificadions

--- spyro/solvers/__init__.py
+++ spyro/solvers/__init__.py
@@ -1,11 +1,11 @@
 from .leapfrog import Leapfrog
-from .leapfrog_AD import Leapfrog_AD
+from .forward_AD import forward_AD
 from .leapfrog_adjoint import Leapfrog_adjoint
 from .ssprk3 import SSPRK3
 
 __all__ = [
     "Leapfrog",    #forward solver adapted for Discrete Adjoint
-    "Leapfrog_AD", #forward solver adapted for Automatic Differentiation
+    "forward_AD", #forward solver adapted for Automatic Differentiation
     "Leapfrog_adjoint",
     "SSPRK3",
 ]
