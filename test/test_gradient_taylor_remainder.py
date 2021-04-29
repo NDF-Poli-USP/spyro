db35de74d65ed810ce9b1ea79eccbfad3e2794f5
Keith Roberts <krober@usp.edu>
2021-04-29 18:25:30 -0300

    cleaning up gradient test

--- test/test_gradient_taylor_remainder.py
+++ test/test_gradient_taylor_remainder.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 
 from firedrake import *
@@ -8,7 +7,11 @@ import spyro
 from .inputfiles.Model1_Leapfrog_adjoint_2d import model
 
 
-outfile_total_gradient = File(os.getcwd() + "/results/Gradient.pvd")
+# outfile_total_gradient = File(os.getcwd() + "/results/Gradient.pvd")
+
+forward = spyro.solvers.Leapfrog
+adjoint = spyro.solvers.Leapfrog_adjoint
+functional = spyro.utils.compute_functional
 
 
 def _make_vp_exact(V, mesh):
@@ -29,44 +32,8 @@ def _make_vp_guess(V, mesh):
     return vp_guess
 
 
-def _simulate_exact(model, mesh, comm, vp_exact, sources, receivers):
-    """Simulate the observed data"""
-    p_exact, p_exact_recv = spyro.solvers.Leapfrog(
-        model, mesh, comm, vp_exact, sources, receivers, output=False
-    )
-    return p_exact, p_exact_recv
-
-
-def _simulate_guess(model, mesh, comm, vp_guess, sources, receivers):
-    """Simulate the guess data"""
-    p_guess, p_guess_recv = spyro.solvers.Leapfrog(
-        model, mesh, comm, vp_guess, sources, receivers, output=False
-    )
-    return p_guess, p_guess_recv
-
-
-def _compute_misfit(model, mesh, comm, guess, exact):
-    """Compute the residual"""
-    misfit = spyro.utils.evaluate_misfit(model, comm, guess, exact)
-    return misfit
-
-
-def _compute_functional(model, mesh, comm, misfit):
-    """Compute the L2 norm functional"""
-    J = spyro.utils.compute_functional(model, comm, misfit)
-    return J
-
-
-def _compute_gradient(model, mesh, comm, vp_guess, receivers, misfit, p_guess):
-    """"Compute the gradient of the functional and the functional"""
-    grad = spyro.solvers.Leapfrog_adjoint(
-        model, mesh, comm, vp_guess, receivers, p_guess, misfit
-    )
-    outfile_total_gradient.write(grad, name="TotalGradient")
-    return grad
-
-
 def test_gradient_talyor_remainder():
+    """2nd order Taylor remainder test"""
 
     comm = spyro.utils.mpi_init(model)
 
@@ -81,52 +48,64 @@ def test_gradient_talyor_remainder():
     receivers = spyro.Receivers(model, mesh, V, comm).create()
 
     # simulate the exact model
-    _, p_exact_recv = _simulate_exact(model, mesh, comm, vp_exact, sources, receivers)
-
-    # simulate the guess model
-    p_guess, p_guess_recv = _simulate_guess(
-        model, mesh, comm, vp_guess, sources, receivers
+    p_exact, p_exact_recv = forward(
+        model,
+        mesh,
+        comm,
+        vp_exact,
+        sources,
+        receivers,
     )
 
-    # compute the misfit
-    misfit = _compute_misfit(model, mesh, comm, p_guess_recv, p_exact_recv)
+    # simulate the guess model
+    p_guess, p_guess_recv = forward(
+        model,
+        mesh,
+        comm,
+        vp_guess,
+        sources,
+        receivers,
+    )
+
+    misfit = p_exact_recv - p_guess_recv
+
+    Jm = functional(model, comm, misfit)
 
     # compute the gradient of the control (to be verified)
-    grad = _compute_gradient(model, mesh, comm, vp_guess, receivers, misfit, p_guess)
+    dJ = adjoint(model, mesh, comm, vp_guess, receivers, p_guess, misfit)
 
-    # compute the functional
-    J = []
-    J.append(_compute_functional(model, mesh, comm, misfit))
+    step = 0.01  # step length
 
-    delta_m = Function(V).assign(0.50)
-    step = 0.01  #
+    delta_m = Function(V)  # model direction (random)
+    delta_m.vector()[:] = np.random.rand(V.dim())
 
-    remainder = []
-    for i in range(3):
-        vp_guess = _make_vp_guess(V, mesh)
+    remainders = []
+    steps = []
+    # this deepcopy is important otherwise pertubations accumulate
+    vp_original = vp_guess.copy(deepcopy=True)
+    for i in range(4):
+        steps.append(step)
         # perturb the model and calculate the functional (again)
         # J(m + delta_m*h)
-        vp_guess.dat.data[:] += step * delta_m.dat.data[:]
-        # simulate the guess model with the new vp_guess
-        _, p_guess_recv = _simulate_guess(
-            model, mesh, comm, vp_guess, sources, receivers
-        )
-        # misfit calculation
-        misfit = _compute_misfit(model, mesh, comm, p_guess_recv, p_exact_recv)
-        # compute the functional (again)
-        J.append(_compute_functional(model, mesh, comm, misfit))
+        vp_guess = vp_original + step * delta_m
+        _, p_guess_recv = forward(model, mesh, comm, vp_guess, sources, receivers)
+        Jp = functional(model, comm, p_exact_recv - p_guess_recv)
         # compute the second-order Taylor remainder
-        remainder.append(J[i + 1] - J[0] - step * assemble(grad * delta_m * dx))
-        # assemble(grad * delta_m * dx))
+        remainder = np.abs(Jp - Jm - step * np.dot(dJ.vector(), delta_m.vector()))
+        remainders.append(remainder)
         # halve the step and repeat
         step /= 2.0
 
     # remainder should decrease at a second order rate
-    remainder = np.array(np.abs(remainder))
-    l2conv = np.log2(remainder[:-1] / remainder[1:])
-    # print(remainder)
-    print(l2conv)
-    assert (l2conv > 1.8).all()
+    remainders = np.array(remainders)
+    print(remainders)
+    r = []
+    for i in range(1, len(steps)):
+        r.append(
+            np.log(remainders[i] / remainders[i - 1]) / np.log(steps[i] / steps[i - 1])
+        )
+    print("Computed convergence rates: {}".format(r))
+    assert r[-1] > 1.8
 
 
 if __name__ == "__main__":
