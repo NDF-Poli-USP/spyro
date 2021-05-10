963704e2461c85f8594f59efd1522223ad4f87ae
Keith Roberts <krober@usp.edu>
2021-05-10 11:50:41 -0300

    remove decprecated function

--- spyro/solvers/__init__.py
+++ spyro/solvers/__init__.py
@@ -1,10 +1,9 @@
-from .forward import forward, ensemble
+from .forward import forward
 from .forward_AD import forward_AD
 from .gradient import gradient
 
 __all__ = [
     "forward",  # forward solver adapted for discrete adjoint
-    "ensemble",
     "forward_AD",  # forward solver adapted for Automatic Differentiation
     "gradient",
 ]
