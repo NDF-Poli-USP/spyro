8c36deb8a77f75285cea9d26c2a73a0b60989c7c
Alexandre Olender <olender@usp.br>
2021-11-08 09:07:05 -0300

    Finished smooth function that saves segy

--- spyro/utils/synthetic.py
+++ spyro/utils/synthetic.py
@@ -1,6 +1,54 @@
-def smooth_field():
-    return vp
+import os
+from scipy.ndimage import gaussian_filter
+import segyio
+import numpy as np
+import matplotlib.pyplot as plt
+import firedrake as fire
 
-def create_shot_record():
-    return outputfile
+def smooth_field(input_filename, output_filename, show = False):
+    f, filetype = os.path.splitext(input_filename)
+
+    if filetype == ".segy":
+        with segyio.open(input_filename, ignore_geometry=True) as f:
+            nz, nx = len(f.samples), len(f.trace)
+            vp = np.zeros(shape=(nz, nx))
+            for index, trace in enumerate(f.trace):
+                vp[:, index] = trace
+
+    sigma = 100
+    vp_smooth = gaussian_filter(vp, sigma)
+
+    spec = segyio.spec()
+    spec.sorting = 2 # not sure what this means
+    spec.format = 1 # not sure what this means
+    spec.samples = range(vp_smooth.shape[0])
+    spec.ilines = range(vp_smooth.shape[1])
+    spec.xlines = range(vp_smooth.shape[0])
+
+    assert np.sum(np.isnan(vp_smooth[:])) == 0
+
+    with segyio.create(output_filename, spec) as f:
+        for tr, il in enumerate(spec.ilines):
+            f.trace[tr] = vp_smooth[:, tr]
+
+    if show == True:
+        with segyio.open(output_filename, ignore_geometry=True) as f:
+            nz, nx = len(f.samples), len(f.trace)
+            show_vp = np.zeros(shape=(nz, nx))
+            for index, trace in enumerate(f.trace):
+                show_vp[:, index] = trace
+            
+        fig, ax = plt.subplots()
+        plt.pcolormesh(show_vp, shading="auto")
+        plt.title("Guess model")
+        plt.colorbar(label="P-wave velocity (km/s)")
+        plt.xlabel("x-direction (m)")
+        plt.ylabel("z-direction (m)")
+        ax.axis("equal")
+        plt.show()
+
+    return None
+
+# def create_shot_record():
+#     return outputfile
 
