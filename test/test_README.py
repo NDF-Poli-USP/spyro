63ed81885975899313e6c17704f7e27fc9870cc1
Alexandre Olender <olender@usp.br>
2022-01-27 13:25:37 -0300

    this shouldn't be commented out

--- test/test_README.py
+++ test/test_README.py
@@ -6,10 +6,10 @@ import pytest
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-# @pytest.mark.parametrize(
-#     "string,lineno",
-#     exdown.extract(this_dir.parent / "README.md", syntax_filter="python"),
-# )
+@pytest.mark.parametrize(
+    "string,lineno",
+    exdown.extract(this_dir.parent / "README.md", syntax_filter="python"),
+)
 def test_readme(string, lineno):
     try:
         # https://stackoverflow.com/a/62851176/353337
