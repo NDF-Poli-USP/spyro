6bb3c46063e62f9c4cd9dea2031e6c7d4c61a3ac
olender <olender@usp.br>
2022-04-12 08:37:52 -0300

    Seperated mesh gen from grid point calc test

--- test/test_tools.py
+++ test/test_tools.py
@@ -26,6 +26,38 @@ def triangle_area(p1, p2, p3):
 
     return abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
 
+def test_mesh_generation_for_grid_calc():
+    grid_point_calculator_parameters = {
+        ## Experiment parameters
+        'source_frequency' : 5.0, # Here we define the frequency of the Ricker wavelet source
+        'minimum_velocity_in_the_domain' :  1.429, # The minimum velocity present in the domain.
+        # if an homogeneous test case is used this velocity will be defined in the whole domain.
+        'velocity_profile_type': 'homogeneous', # Either or heterogeneous. If heterogeneous is 
+        #chosen be careful to have the desired velocity model below.
+        'velocity_model_file_name': 'vel_z6.25m_x12.5m_exact.segy',
+        'FEM_method_to_evaluate' : 'KMV', # FEM to evaluate such as `KMV` or `spectral` (GLL nodes on quads and hexas)
+        'dimension' : 2, # Domain dimension. Either 2 or 3.
+        'receiver_setup' : 'near', #Either near or line. Near defines a receiver grid near to the source,
+        # line defines a line of point receivers with pre-established near and far offsets.
+
+        ## Line search parameters
+        'reference_degree': 4, # Degree to use in the reference case (int)
+        'G_reference': 8.0, # grid point density to use in the reference case (float)
+        'desired_degree': 4, # degree we are calculating G for. (int)
+        'G_initial': 7.0, # Initial G for line search (float)
+        'accepted_error_threshold': 0.07, 
+        'g_accuracy': 1e-1
+        }
+    Gs = [7.0, 7.1, 7.7, 8.0]
+    degree_reference = grid_point_calculator_parameters['reference_degree']
+
+    model = spyro.tools.create_model_for_grid_point_calculation(grid_point_calculator_parameters, degree_reference)
+    comm = spyro.utils.mpi_init(model)
+    for G in Gs:
+        model["mesh"]["meshfile"] = "meshes/2Dhomogeneous"+str(G)+".msh"
+        model = spyro.tools.generate_mesh(model, G, comm)
+
+
 def test_grid_calc2d():
     grid_point_calculator_parameters = {
         ## Experiment parameters
@@ -165,4 +197,5 @@ def test_input_models_receivers():
 
 
 if __name__ == "__main__":
+    test_mesh_generation_for_grid_calc()
     test_grid_calc2d()
