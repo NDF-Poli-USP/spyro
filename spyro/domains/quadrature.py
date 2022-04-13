3b4de14dc130aac10f485359acaad04c00a96294
Alexandre Olender <olender@usp.br>
2022-04-13 12:05:16 -0300

    Commenting out unused code

--- spyro/domains/quadrature.py
+++ spyro/domains/quadrature.py
@@ -27,11 +27,11 @@ def quadrature_rules(V):
         qr_s = gauss_lobatto_legendre_cube_rule(
             dimension=(dimension - 1), degree=degree
         )
-    elif (cell_geometry == quadrilateral) and ufl_method == "DQ":
-        # In this case, we use GL quadrature
-        qr_x = gauss_legendre_cube_rule(dimension=dimension, degree=degree)
-        qr_k = qr_x
-        qr_s = gauss_legendre_cube_rule(dimension=(dimension - 1), degree=degree)
+    # elif (cell_geometry == quadrilateral) and ufl_method == "DQ":
+    #     # In this case, we use GL quadrature
+    #     qr_x = gauss_legendre_cube_rule(dimension=dimension, degree=degree)
+    #     qr_k = qr_x
+    #     qr_s = gauss_legendre_cube_rule(dimension=(dimension - 1), degree=degree)
     elif (cell_geometry == triangle) and (
         ufl_method == "Lagrange" or ufl_method == "Discontinuous Lagrange"
     ):
@@ -79,19 +79,19 @@ def gauss_lobatto_legendre_cube_rule(dimension, degree):
 # -------------------------- #
 # Spectral method - Gauss-Legendre rule
 # 1D
-def gauss_legendre_line_rule(degree):
-    fiat_make_rule = FIAT.quadrature.GaussLegendreQuadratureLineRule
-    fiat_rule = fiat_make_rule(FIAT.ufc_simplex(1), degree + 1)
-    finat_ps = finat.point_set.GaussLegendrePointSet
-    finat_qr = finat.quadrature.QuadratureRule
-    return finat_qr(finat_ps(fiat_rule.get_points()), fiat_rule.get_weights())
+# def gauss_legendre_line_rule(degree):
+#     fiat_make_rule = FIAT.quadrature.GaussLegendreQuadratureLineRule
+#     fiat_rule = fiat_make_rule(FIAT.ufc_simplex(1), degree + 1)
+#     finat_ps = finat.point_set.GaussLegendrePointSet
+#     finat_qr = finat.quadrature.QuadratureRule
+#     return finat_qr(finat_ps(fiat_rule.get_points()), fiat_rule.get_weights())
 
 
-# 3D
-def gauss_legendre_cube_rule(dimension, degree):
-    make_tensor_rule = finat.quadrature.TensorProductQuadratureRule
-    result = gauss_legendre_line_rule(degree)
-    for _ in range(1, dimension):
-        line_rule = gauss_legendre_line_rule(degree)
-        result = make_tensor_rule([result, line_rule])
-    return result
+# # 3D
+# def gauss_legendre_cube_rule(dimension, degree):
+#     make_tensor_rule = finat.quadrature.TensorProductQuadratureRule
+#     result = gauss_legendre_line_rule(degree)
+#     for _ in range(1, dimension):
+#         line_rule = gauss_legendre_line_rule(degree)
+#         result = make_tensor_rule([result, line_rule])
+#     return result
