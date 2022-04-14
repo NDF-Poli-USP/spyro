841e6379ff61f7d6c26e7664c648a3d907e2c6e7
olender <olender@usp.br>
2022-04-14 12:31:25 -0300

    testing receivers for heterogeneous models

--- test/test_tools.py
+++ test/test_tools.py
@@ -198,6 +198,108 @@ def test_input_models_receivers():
 
     assert all([test1, test2])
 
+@pytest.mark.mpi_skip()
+def test_input_models_receivers_heterogeneous():
+    test1 = True #testing if 2D receivers bins are inside the domain on an heterogeneous case
+    grid_point_calculator_parameters = {
+        ## Experiment parameters
+        'source_frequency' : 5.0, # Here we define the frequency of the Ricker wavelet source
+        'minimum_velocity_in_the_domain' :  1.429, # The minimum velocity present in the domain.
+        # if an homogeneous test case is used this velocity will be defined in the whole domain.
+        'velocity_profile_type': 'heterogeneous', # Either or heterogeneous. If heterogeneous is 
+        #chosen be careful to have the desired velocity model below.
+        'FEM_method_to_evaluate' : 'KMV', # FEM to evaluate such as `KMV` or `spectral` (GLL nodes on quads and hexas)
+        'dimension' : 2, # Domain dimension. Either 2 or 3.
+        'receiver_type' : 'bins', #Either near or line. Near defines a receiver grid near to the source,
+        # line defines a line of point receivers with pre-established near and far offsets.
+
+        ## Line search parameters
+        'reference_degree': 4, # Degree to use in the reference case (int)
+        'G_reference': 8.0, # grid point density to use in the reference case (float)
+        'desired_degree': 4, # degree we are calculating G for. (int)
+        'G_initial': 7.0, # Initial G for line search (float)
+        'accepted_error_threshold': 0.05, 
+        'g_accuracy': 1e-1
+        }
+    model = spyro.tools.create_model_for_grid_point_calculation(grid_point_calculator_parameters, 4)
+
+    Lz = model["mesh"]['Lz']
+    lz = model['BCs']['lz']
+    Lx = model["mesh"]['Lx']
+    lx = model['BCs']['lx']
+
+    Real_Lz = Lz + lz
+    Real_Lx = Lx + 2*lx
+
+    p1 = (-Real_Lz,-lx)
+    p2 = (-Real_Lz,Real_Lx-lx)
+    p3 = (0.0,-lx)
+    p4 = (0.0,Real_Lx-lx)
+
+    areaSquare = Real_Lz*Real_Lx
+
+    for r in model["acquisition"]["receiver_locations"]:
+        area1 = triangle_area(p1, p2, r)
+        area2 = triangle_area(p1, p3, r)
+        area3 = triangle_area(p3, p4, r)
+        area4 = triangle_area(p2, p4, r)
+        test = math.isclose((area1 + area2 + area3 + area4), areaSquare, rel_tol=1e-09)
+        if test == False:
+            test1 = False
+    
+    test2 = True #testing if 2D receivers line are inside the domain on an heterogeneous case
+    grid_point_calculator_parameters = {
+        ## Experiment parameters
+        'source_frequency' : 5.0, # Here we define the frequency of the Ricker wavelet source
+        'minimum_velocity_in_the_domain' :  1.429, # The minimum velocity present in the domain.
+        # if an homogeneous test case is used this velocity will be defined in the whole domain.
+        'velocity_profile_type': 'heterogeneous', # Either or heterogeneous. If heterogeneous is 
+        #chosen be careful to have the desired velocity model below.
+        'FEM_method_to_evaluate' : 'KMV', # FEM to evaluate such as `KMV` or `spectral` (GLL nodes on quads and hexas)
+        'dimension' : 2, # Domain dimension. Either 2 or 3.
+        'receiver_type' : 'bins', #Either near or line. Near defines a receiver grid near to the source,
+        # line defines a line of point receivers with pre-established near and far offsets.
+
+        ## Line search parameters
+        'reference_degree': 4, # Degree to use in the reference case (int)
+        'G_reference': 8.0, # grid point density to use in the reference case (float)
+        'desired_degree': 4, # degree we are calculating G for. (int)
+        'G_initial': 7.0, # Initial G for line search (float)
+        'accepted_error_threshold': 0.05, 
+        'g_accuracy': 1e-1
+        }
+    model = spyro.tools.create_model_for_grid_point_calculation(grid_point_calculator_parameters, 4)
+
+    Lz = model["mesh"]['Lz']
+    lz = model['BCs']['lz']
+    Lx = model["mesh"]['Lx']
+    lx = model['BCs']['lx']
+
+    Real_Lz = Lz + lz
+    Real_Lx = Lx + 2*lx
+
+    p1 = (-Real_Lz,-lx)
+    p2 = (-Real_Lz,Real_Lx-lx)
+    p3 = (0.0,-lx)
+    p4 = (0.0,Real_Lx-lx)
+
+    areaSquare = Real_Lz*Real_Lx
+
+    for r in model["acquisition"]["receiver_locations"]:
+        area1 = triangle_area(p1, p2, r)
+        area2 = triangle_area(p1, p3, r)
+        area3 = triangle_area(p3, p4, r)
+        area4 = triangle_area(p2, p4, r)
+        test = math.isclose((area1 + area2 + area3 + area4), areaSquare, rel_tol=1e-09)
+        if test == False:
+            test2 = False
+
+
+
+    assert all([test1, test2])
+
+
+
 
 if __name__ == "__main__":
     test_mesh_generation_for_grid_calc()
