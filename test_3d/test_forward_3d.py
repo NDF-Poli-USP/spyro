3c72fd26ef773b92220279b5c8ce32347e825cea
olender <olender@usp.br>
2022-04-15 19:33:05 -0300

    Adding 3D test

--- test_3d/test_forward_3d.py
+++ test_3d/test_forward_3d.py
@@ -8,45 +8,24 @@ import pytest
 def compare_velocity(p_r, receiver_in_source_index, receiver_comparison_index, model,dt):
     receiver_0 = p_r[:,receiver_in_source_index]
     receiver_1 = p_r[:,receiver_comparison_index]
+
     pos = model["acquisition"]["receiver_locations"]
+
     time0 = np.argmax(receiver_0)*dt
     time1 = np.argmax(receiver_1)*dt
-    x0 = pos[receiver_in_source_index,1]
-    x1 = pos[receiver_comparison_index,1]
+
+    x0 = pos[receiver_in_source_index][1]
+    x1 = pos[receiver_comparison_index][1]
+
     measured_velocity = np.abs(x1-x0)/(time1-time0)
     minimum_velocity = 1.5
+
     error_percent = 100*np.abs(measured_velocity-minimum_velocity)/minimum_velocity
-    print(f"Velocity error of {error_percent}%.", flush = True)
     return error_percent
-
-def get_receiver_in_source_location(source_id, model):
-    receiver_locations = model["acquisition"]["receiver_locations"]
-    source_locations = model["acquisition"]["source_pos"]
-    source_x = source_locations[source_id,1]
-
-    cont = 0
-    for receiver_location in receiver_locations:
-        if math.isclose(source_x, receiver_location[1]):
-            return cont
-        cont += 1
-    return ValueError("Couldn't find a receiver whose location coincides with a source within the standard tolerance.")
-
 
 
 @pytest.mark.mpi(min_size=15)
 def test_forward_3d():
-    line1 = spyro.create_transect((0.25, 0.25), (0.25, 7.25), 4)
-    line2 = spyro.create_transect((2.0, 0.25), (2.0, 7.25), 4)
-    line3 = spyro.create_transect((3.75, 0.25), (3.75, 7.25), 4)
-    line4 = spyro.create_transect((5.5, 0.25), (5.25, 7.25), 4)
-    line5 = spyro.create_transect((7.25, 0.25), (7.25, 7.25), 4)
-    lines = np.concatenate((line1, line2, line3, line4, line5))
-
-    sources = spyro.insert_fixed_value(lines, -0.10, 0)
-
-    receivers = spyro.create_2d_grid(0.25, 7.25, 0.25, 7.25, 30)
-    receivers = spyro.insert_fixed_value(receivers, -0.15, 0)
-
     model = {}
 
     model["opts"] = {
@@ -77,24 +56,30 @@ def test_forward_3d():
     }
     model["acquisition"] = {
         "source_type": "Ricker",
-        "source_pos": sources,
+        "source_pos": [(-0.15, 0.25, 0.25)],
         "frequency": 5.0,
         "delay": 1.0,
-        "receiver_locations": receivers,
+        "receiver_locations": [(-0.15, 0.25, 0.25), (-0.15, 0.3, 0.25), (-0.15, 0.35, 0.25), (-0.15, 0.4, 0.25), (-0.15, 0.45, 0.25), (-0.15, 0.5, 0.25), (-0.15, 0.55, 0.25), (-0.15, 0.6, 0.25)],
+    }
+    model["aut_dif"] ={
+        "status": False
     }
     model["timeaxis"] = {
         "t0": 0.0,  #  Initial time for event
-        "tf": 2.00,  # Final time for event
+        "tf": 0.6,  # Final time for event
         "dt": 0.00075,
         "amplitude": 1,  # the Ricker has an amplitude of 1.
         "nspool": 100,  # how frequently to output solution to pvds
         "fspool": 99999,  # how frequently to save solution to RAM
     }
+
     comm = spyro.utils.mpi_init(model)
     mesh, V = spyro.io.read_mesh(model, comm)
     vp = spyro.io.interpolate(model, mesh, V, guess=False)
+
     if comm.ensemble_comm.rank == 0:
         File("true_velocity.pvd", comm=comm.comm).write(vp)
+
     sources = spyro.Sources(model, mesh, V, comm)
     receivers = spyro.Receivers(model, mesh, V, comm)
     wavelet = spyro.full_ricker_wavelet(
@@ -107,3 +92,17 @@ def test_forward_3d():
         model, mesh, comm, vp, sources, wavelet, receivers, output=False
     )
 
+
+    dt=model["timeaxis"]["dt"]
+    final_time=model["timeaxis"]["tf"]
+
+    pass_error_test = True
+
+    if comm.comm.rank == 0:
+        error_percent = compare_velocity(p_r, 0, 7, model,dt)
+        if error_percent < 5:
+            pass_error_test = True
+        else:
+            pass_error_test = False
+    
+    assert pass_error_test
