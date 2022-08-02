5eb8448a1f645d7bb32e16428eb1131fb06e216f
olender <olender@usp.br>
2022-08-02 10:49:03 -0300

    uncommented default dictionary

--- spyro/io/model_parameters.py
+++ spyro/io/model_parameters.py
@@ -5,94 +5,94 @@ import spyro
 import firedrake as fire
 import os.path
 
-# default_optimization_parameters = {
-#     "General": {"Secant": {"Type": "Limited-Memory BFGS", "Maximum Storage": 10}},
-#     "Step": {
-#         "Type": "Augmented Lagrangian",
-#         "Augmented Lagrangian": {
-#             "Subproblem Step Type": "Line Search",
-#             "Subproblem Iteration Limit": 5.0,
-#         },
-#         "Line Search": {"Descent Method": {"Type": "Quasi-Newton Step"}},
-#     },
-#     "Status Test": {
-#         "Gradient Tolerance": 1e-16,
-#         "Iteration Limit": None,
-#         "Step Tolerance": 1.0e-16,
-#     },
-# }
+default_optimization_parameters = {
+    "General": {"Secant": {"Type": "Limited-Memory BFGS", "Maximum Storage": 10}},
+    "Step": {
+        "Type": "Augmented Lagrangian",
+        "Augmented Lagrangian": {
+            "Subproblem Step Type": "Line Search",
+            "Subproblem Iteration Limit": 5.0,
+        },
+        "Line Search": {"Descent Method": {"Type": "Quasi-Newton Step"}},
+    },
+    "Status Test": {
+        "Gradient Tolerance": 1e-16,
+        "Iteration Limit": None,
+        "Step Tolerance": 1.0e-16,
+    },
+}
 
-# default_dictionary = {}
-# default_dictionary["options"] = {
-#     "cell_type": "T",  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
-#     "variant": 'lumped', # lumped, equispaced or DG, default is lumped
-#     "method": "MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
-#     "degree": 4,  # p order
-#     "dimension": 2,  # dimension
-# }
+default_dictionary = {}
+default_dictionary["options"] = {
+    "cell_type": "T",  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
+    "variant": 'lumped', # lumped, equispaced or DG, default is lumped
+    "method": "MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
+    "degree": 4,  # p order
+    "dimension": 2,  # dimension
+}
 
-# # Number of cores for the shot. For simplicity, we keep things serial.
-# # spyro however supports both spatial parallelism and "shot" parallelism.
-# default_dictionary["parallelism"] = {
-#     "type": "automatic",  # options: automatic (same number of cores for evey processor) or spatial
-# }
+# Number of cores for the shot. For simplicity, we keep things serial.
+# spyro however supports both spatial parallelism and "shot" parallelism.
+default_dictionary["parallelism"] = {
+    "type": "automatic",  # options: automatic (same number of cores for evey processor) or spatial
+}
 
-# # Define the domain size without the PML. Here we'll assume a 0.75 x 1.50 km
-# # domain and reserve the remaining 250 m for the Perfectly Matched Layer (PML) to absorb
-# # outgoing waves on three sides (eg., -z, +-x sides) of the domain.
-# default_dictionary["mesh"] = {
-#     "Lz": 1.0,  # depth in km - always positive
-#     "Lx": 1.0,  # width in km - always positive
-#     "Ly": 0.0,  # thickness in km - always positive
-#     "mesh_file": None,
-# }
-# default_dictionary["synthetic_data"] = {    #For use only if you are using a synthetic test model or a forward only simulation
-#     "real_mesh_file": None,
-#     "real_velocity_file": None,
-# }
-# default_dictionary["inversion"] = {
-#     "perform_fwi": False, # switch to true to make a FWI
-#     "initial_guess_model_file": None,
-#     "shot_record_file": None,
-#     "optimization_parameters": default_optimization_parameters,
-# }
+# Define the domain size without the PML. Here we'll assume a 0.75 x 1.50 km
+# domain and reserve the remaining 250 m for the Perfectly Matched Layer (PML) to absorb
+# outgoing waves on three sides (eg., -z, +-x sides) of the domain.
+default_dictionary["mesh"] = {
+    "Lz": 1.0,  # depth in km - always positive
+    "Lx": 1.0,  # width in km - always positive
+    "Ly": 0.0,  # thickness in km - always positive
+    "mesh_file": None,
+}
+default_dictionary["synthetic_data"] = {    #For use only if you are using a synthetic test model or a forward only simulation
+    "real_mesh_file": None,
+    "real_velocity_file": None,
+}
+default_dictionary["inversion"] = {
+    "perform_fwi": False, # switch to true to make a FWI
+    "initial_guess_model_file": None,
+    "shot_record_file": None,
+    "optimization_parameters": default_optimization_parameters,
+}
 
-# # Specify a 250-m PML on the three sides of the domain to damp outgoing waves.
-# default_dictionary["absorving_boundary_conditions"] = {
-#     "status": False,  # True or false
-#     "outer_bc": "non-reflective",  #  None or non-reflective (outer boundary condition)
-#     "damping_type": "polynomial",  # polynomial, hyperbolic, shifted_hyperbolic
-#     "exponent": 2,  # damping layer has a exponent variation
-#     "cmax": 4.7,  # maximum acoustic wave velocity in PML - km/s
-#     "R": 1e-6,  # theoretical reflection coefficient
-#     "lz": 0.25,  # thickness of the PML in the z-direction (km) - always positive
-#     "lx": 0.25,  # thickness of the PML in the x-direction (km) - always positive
-#     "ly": 0.0,  # thickness of the PML in the y-direction (km) - always positive
-# }
+# Specify a 250-m PML on the three sides of the domain to damp outgoing waves.
+default_dictionary["absorving_boundary_conditions"] = {
+    "status": False,  # True or false
+    "outer_bc": "non-reflective",  #  None or non-reflective (outer boundary condition)
+    "damping_type": "polynomial",  # polynomial, hyperbolic, shifted_hyperbolic
+    "exponent": 2,  # damping layer has a exponent variation
+    "cmax": 4.7,  # maximum acoustic wave velocity in PML - km/s
+    "R": 1e-6,  # theoretical reflection coefficient
+    "lz": 0.25,  # thickness of the PML in the z-direction (km) - always positive
+    "lx": 0.25,  # thickness of the PML in the x-direction (km) - always positive
+    "ly": 0.0,  # thickness of the PML in the y-direction (km) - always positive
+}
 
-# # Create a source injection operator. Here we use a single source with a
-# # Ricker wavelet that has a peak frequency of 8 Hz injected at the center of the mesh.
-# # We also specify to record the solution at 101 microphones near the top of the domain.
-# # This transect of receivers is created with the helper function `create_transect`.
-# default_dictionary["acquisition"] = {
-#     "source_type": "ricker",
-#     "source_locations": [(-0.1, 0.5)],
-#     "frequency": 5.0,
-#     "delay": 1.0,
-#     "receiver_locations": spyro.create_transect(
-#         (-0.10, 0.1), (-0.10, 0.9), 20
-#     ),
-# }
+# Create a source injection operator. Here we use a single source with a
+# Ricker wavelet that has a peak frequency of 8 Hz injected at the center of the mesh.
+# We also specify to record the solution at 101 microphones near the top of the domain.
+# This transect of receivers is created with the helper function `create_transect`.
+default_dictionary["acquisition"] = {
+    "source_type": "ricker",
+    "source_locations": [(-0.1, 0.5)],
+    "frequency": 5.0,
+    "delay": 1.0,
+    "receiver_locations": spyro.create_transect(
+        (-0.10, 0.1), (-0.10, 0.9), 20
+    ),
+}
 
-# # Simulate for 2.0 seconds.
-# default_dictionary["time_axis"] = {
-#     "initial_time": 0.0,  #  Initial time for event
-#     "final_time": 2.00,  # Final time for event
-#     "dt": 0.001,  # timestep size
-#     "amplitude": 1,  # the Ricker has an amplitude of 1.
-#     "nspool": 100,  # how frequently to output solution to pvds
-#     "fspool": 100,  # how frequently to save solution to RAM
-# }
+# Simulate for 2.0 seconds.
+default_dictionary["time_axis"] = {
+    "initial_time": 0.0,  #  Initial time for event
+    "final_time": 2.00,  # Final time for event
+    "dt": 0.001,  # timestep size
+    "amplitude": 1,  # the Ricker has an amplitude of 1.
+    "nspool": 100,  # how frequently to output solution to pvds
+    "fspool": 100,  # how frequently to save solution to RAM
+}
 
 class model_parameters:
     def __init__(self, dictionary=default_dictionary):
@@ -282,6 +282,7 @@ class model_parameters:
                 method = 'CG_quadrilateral'
             elif variant == 'DG':
                 method = 'DG_quadrilateral'
+        self.method = method
 
     def __get_method(self):
         dictionary = self.input_dictionary
