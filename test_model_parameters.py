827284a9fc2e141b25d8fc96bff0300995785a21
olender <olender@usp.br>
2022-08-02 10:49:26 -0300

    added initial input tests

--- test_model_parameters.py
+++ test_model_parameters.py
@@ -1,11 +1,12 @@
 import spyro
 from spyro.io.model_parameters import model_parameters
+from copy import deepcopy
 
 dictionary = {}
 dictionary["options"] = {
     "cell_type": "T",  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
     "variant": 'lumped', # lumped, equispaced or DG, default is lumped
-    "method": "MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
+    "method":"MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
     "degree": 4,  # p order
     "dimension": 2,  # dimension
 }
@@ -30,9 +31,9 @@ dictionary["synthetic_data"] = {    #For use only if you are using a synthetic t
     "real_velocity_file": None,
 }
 dictionary["inversion"] = {
+    "perform_fwi": False, # switch to true to make a FWI
     "initial_guess_model_file": None,
     "shot_record_file": None,
-    "optimization_parameters": None,
 }
 
 # Specify a 250-m PML on the three sides of the domain to damp outgoing waves.
@@ -72,6 +73,131 @@ dictionary["time_axis"] = {
     "fspool": 100,  # how frequently to save solution to RAM
 }
 
-model_parameters(dictionary=dictionary)
+def test_method_reader():
+    test_dictionary = deepcopy(dictionary)
+    test_dictionary["options"] = {
+    "cell_type": None,  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
+    "variant": None, # lumped, equispaced or DG, default is lumped
+    "method": None, # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
+    "degree": 4,  # p order
+    "dimension": 2,  # dimension
+    }
+    # Trying out different method entries and seeing if all of them work for MLT
+    test1 = False
+    test_dictionary["options"]["method"] = 'MLT'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'mass_lumped_triangle':
+        test1 = True
+
+    test2 = False
+    test_dictionary["options"]["method"] = 'KMV'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'mass_lumped_triangle':
+        test2 = True
+
+    test3 = False
+    test_dictionary["options"]["method"] = 'mass_lumped_triangle'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'mass_lumped_triangle':
+        test3 = True
+
+    # Trying out different method entries for spectral quads
+    test4 = False
+    test_dictionary["options"]["method"] = 'spectral_quadrilateral'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'spectral_quadrilateral':
+        test4 = True
+
+    test5 = False
+    test_dictionary["options"]["method"] = 'CG'
+    test_dictionary["options"]["variant"] = 'GLL'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'spectral_quadrilateral':
+        test5 = True
+    
+    test6 = False
+    test_dictionary["options"]["method"] = 'SEM'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'spectral_quadrilateral':
+        test6 = True
+    
+    #Trying out some entries for other less used methods
+    test7 = False
+    test_dictionary["options"]["method"] = 'DG_triangle'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'DG_triangle':
+        test7 = True
+    
+    test8 = False
+    test_dictionary["options"]["method"] = 'DG_quadrilateral'
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'DG_quadrilateral':
+        test8 = True
+    
+    test9 = False
+    test_dictionary["options"]["method"] = 'CG'
+    test_dictionary["options"]["variant"] = None
+    model = model_parameters(dictionary=test_dictionary)
+    if model.method == 'CG':
+        test9 = True
+    
+    assert all([test1, test2, test3,test4, test5, test6, test7, test8, test9])
+    
+def test_cell_type_reader():
+    ct_dictionary = deepcopy(dictionary)
+    ct_dictionary["options"] = {
+    "cell_type": None,  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
+    "variant": None, # lumped, equispaced or DG, default is lumped
+    "method": None, # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
+    "degree": 4,  # p order
+    "dimension": 2,  # dimension
+    }
+    # Testing lumped cases
+    ct_dictionary["options"]["variant"] = 'lumped'
+
+    test1 = False
+    ct_dictionary["options"]["cell_type"] = 'triangle'
+    model = model_parameters(dictionary=ct_dictionary)
+    if model.method == 'mass_lumped_triangle':
+        test1 = True
+    
+    test2 = False
+    ct_dictionary["options"]["cell_type"] = 'quadrilateral'
+    model = model_parameters(dictionary=ct_dictionary)
+    if model.method == 'spectral_quadrilateral':
+        test2 = True
+    
+    # Testing equispaced cases
+    ct_dictionary["options"]["variant"] = 'equispaced'
+
+    test3 = False
+    ct_dictionary["options"]["cell_type"] = 'triangle'
+    model = model_parameters(dictionary=ct_dictionary)
+    if model.method == 'CG_triangle':
+        test3 = True
+    
+    test4 = False
+    ct_dictionary["options"]["cell_type"] = 'quadrilateral'
+    model = model_parameters(dictionary=ct_dictionary)
+    if model.method == 'CG_quadrilateral':
+        test4 = True
+    
+    # Testing DG cases
+    ct_dictionary["options"]["variant"] = 'DG'
+
+    test5 = False
+    ct_dictionary["options"]["cell_type"] = 'triangle'
+    model = model_parameters(dictionary=ct_dictionary)
+    if model.method == 'DG_triangle':
+        test5 = True
+    
+    test6 = False
+    ct_dictionary["options"]["cell_type"] = 'quadrilateral'
+    model = model_parameters(dictionary=ct_dictionary)
+    if model.method == 'DG_quadrilateral':
+        test6 = True
+    
+    assert all([test1, test2, test3, test4, test5, test6])
+
 
 print('END')
