2a1a38d02f08e52aea3c80cce61252cef6b01c05
santos-td <santos.td@gmail.com>
2022-09-21 15:04:41 -0300

    CHG: minor

---
+++ demos/test_inhouse_monge_ampere_solver_with_quads.py
@@ -0,0 +1,76 @@
+from firedrake import *
+from movement import *
+import ufl
+import spyro
+import sys
+import time
+
+distribution_parameters={"partition": True,
+                         "overlap_type": (DistributedMeshOverlapType.VERTEX, 20)}
+n = 40
+mesh = RectangleMesh(n, n, 1, 1, quadrilateral="true", distribution_parameters=distribution_parameters)
+# ring, from cao etal 2002 p. 131 {{{
+den_ring = lambda t, x, y: 1 + t * 5 * exp(-50 * abs( (x-0.5)**2 + (y-0.5)**2 - (0.25)**2 ) )
+#}}}
+# ring2, from mcrae etal 2018, Eq. 5.3 {{{
+den_ring2 = lambda t, x, y: 1 + t * 10 * (1./cosh(200 * ( (x-0.5)**2 + (y-0.5)**2 - (0.25)**2 ) ) )** 2
+#}}}
+# circle, from Spyro run_fwi_acoustic_moving_mesh_circle_case.py {{{
+den_circle = lambda t, x, y: 1.5 / (2.5 + t * tanh(200 * (0.125 - sqrt(( x - 0.5) ** 2 + (y - 0.5) ** 2))))
+#}}}
+
+def monitor_function_ring(mesh, t=1):
+    x,y = SpatialCoordinate(mesh)
+    return den_ring(t,x,y)
+
+def monitor_function_ring2(mesh, t=1):
+    x,y = SpatialCoordinate(mesh)
+    return den_ring2(t,x,y)
+
+def monitor_function_circle(mesh, t=1):
+    x,y = SpatialCoordinate(mesh)
+    return den_circle(t,x,y)
+
+def mask_receivers(mesh):
+    x,y = mesh.coordinates
+    g = conditional(y > 0.9, 0, (1/0.1)*0.9 - (1/0.1)*y) # 0 apply BC
+    g = conditional(y <= 0.8, 1, g)
+    return g
+
+def mask_dummy(mesh):
+    return Constant(1.)
+
+V = FunctionSpace(mesh, "CG", 3)
+f = Function(V)
+f.interpolate( Constant(1.0) ) # to print the mesh 
+
+m = Function(V)
+m.interpolate(monitor_function_ring2(mesh))
+File("monitor.pvd").write(m)
+
+File("mesh_before_amr.pvd").write(f)
+if 1:
+    p = 1
+    tol = 2.0e-03
+
+    ti=time.time()
+    if 1:
+        #spyro.monge_ampere_solver(mesh, monitor_function_ring, p=p, rtol=tol)
+        #spyro.monge_ampere_solver(mesh, monitor_function_ring2, p=p, rtol=tol, mask=None)
+        spyro.monge_ampere_solver(mesh, monitor_function_ring2, p=p, rtol=tol, mask=mask_dummy)
+        #spyro.monge_ampere_solver(mesh, monitor_function_ring2, p=p, rtol=tol, mask=mask_receivers)
+    
+    tf=time.time()
+    print("time in-house solver="+str(tf-ti))
+    File("mesh_after_amr_inhouse_solver.pvd").write(f)
+else:
+    tol = 2.0e-03
+    method = "quasi_newton"
+    ti=time.time()
+    #mover = MongeAmpereMover(mesh, monitor_function_ring, method=method, rtol=tol)
+    mover = MongeAmpereMover(mesh, monitor_function_ring2, method=method, rtol=tol)
+    mover.move()
+    tf=time.time()
+    print("time Joe's solver="+str(tf-ti))
+    File("mesh_after_amr_movement_solver.pvd").write(f)
+
---
+++ demos/test_mesh2mesh_projection4.py
@@ -0,0 +1,90 @@
+# projection vs at
+from firedrake import *
+import weakref
+import spyro
+import time
+import sys
+
+distribution_parameters = {"partition": True,
+                           "overlap_type": (DistributedMeshOverlapType.VERTEX, 50)}
+
+quad=0 # use interpolation_scheme = 3
+if quad:
+    m1 = RectangleMesh(200, 200, 1, 1, quadrilateral=True, distribution_parameters=distribution_parameters)
+    m2 = RectangleMesh(175, 175, 1, 1, quadrilateral=True, distribution_parameters=distribution_parameters)
+else:
+    m1 = RectangleMesh(200, 200, 1, 1, quadrilateral=False, distribution_parameters=distribution_parameters)
+    m2 = RectangleMesh(175, 175, 1, 1, quadrilateral=False, distribution_parameters=distribution_parameters)
+
+p=2
+element = FiniteElement('CG', m1.ufl_cell(), degree=p)
+V1 = FunctionSpace(m1, element)
+V2 = FunctionSpace(m2, element)
+
+m2._parallel_compatible = {weakref.ref(m1)}
+
+x, y = SpatialCoordinate(m1)
+f1 = Function(V1).interpolate(sin(5*x)*cos(5*y))
+f1_integrated = assemble(f1*dx)
+
+f2 = Function(V2)
+
+interpolation_scheme = 3
+if interpolation_scheme==1: # inhouse projection
+    degree=None
+    if V2.ufl_element().family() == "Kong-Mulder-Veldhuizen" and P == 2:
+        degree = 6
+    ti = time.time()
+    spyro.mesh_to_mesh_projection(f1, f2, degree=degree)
+    tf = time.time()
+    print("inhouse projection, time="+str(tf-ti),flush=True)
+elif interpolation_scheme==2: # Firedrake projection:
+    ti = time.time()
+    f2.project(f1)
+    tf = time.time()
+    print("Firedrake projection, time="+str(tf-ti),flush=True)
+elif interpolation_scheme==3: # at scheme
+    ti = time.time()
+    m = V2.ufl_domain()
+    W = VectorFunctionSpace(m, V2.ufl_element())
+    X = interpolate(m.coordinates, W)
+
+    a=X.vector().gather()
+    #x=[]
+    #y=[]
+    #for i in range(len(a)):
+    #    x.append(a[2*i])
+    #    y.append(a[2*i-1])
+
+    x = a[0:len(a):2]
+    y = a[1:len(a):2]
+
+    print(len(x),flush=True)
+    #print(len(y),flush=True)
+    #print(x,flush=True)
+    #print(y,flush=True)
+    print([x,y],flush=True)
+    print(X.dat.data,flush=True)
+    
+    #print(X.dat.data.shape,flush=True)
+    xy = []
+    for i in range(len(x)):
+        xy.append([x[i], y[i]])
+    
+    #print(xy)
+
+
+    #f2.dat.data[:] = f1.at([x[0] , y[0]], dont_raise=True, tolerance=0.001)
+    f2.dat.data[:] = f1.at(xy, dont_raise=True, tolerance=0.001) # FIXME and now? how to set the space (on one partition) with the global values?
+    tf = time.time()
+    print("at scheme, time="+str(tf-ti),flush=True)
+else:
+    sys.exit("interpolation scheme not implemented!")
+    
+f2_integrated = assemble(f2*dx)
+
+print(f1_integrated)
+print(f2_integrated)
+
+File("f1.pvd").write(f1)
+File("f2.pvd").write(f2)
