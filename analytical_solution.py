11e1e0eccea53c0019870a17476a3e5e0c3436f1
olender <olender@usp.br>
2023-06-15 16:30:43 -0300

    Adding analytical solution test

---
+++ analytical_solution.py
@@ -0,0 +1,50 @@
+import numpy as np
+from scipy.special import hankel2
+
+sx, sz = (1.0, 1.0)
+rx, rz = (1.5, 1.5)
+
+def ricker(t_initial, t_final, dt, f):
+    t = np.linspace(t_initial, t_final, int((t_final-t_initial)/dt))
+    tt = (np.pi**2) * (f**2) * (t**2)
+    y = (1.0 - 2.0 * tt) * np.exp(- tt)
+    return y
+
+# def analytical(t_initial, dt, t_final, f0):
+#     # Fourier constants
+#     nf = int(nt/2 + 1)
+#     fnyq = 1. / (2 * dt)
+#     df = 1.0 / t_final
+#     faxis = df * np.arange(nf)
+
+#     wavelet = ricker(f0, t_final, dt, 1.5/f0)
+
+#     # Take the Fourier transform of the source time-function
+#     R = np.fft.fft(wavelet)
+#     R = R[0:nf]
+#     nf = len(R)
+
+#     # Compute the Hankel function and multiply by the source spectrum
+#     U_a = np.zeros((nf), dtype=complex)
+#     for a in range(1, nf-1):
+#         k = 2 * np.pi * faxis[a] / c0
+#         tmp = k * np.sqrt(((rx - sx))**2 + ((rz - sz))**2)
+#         U_a[a] = -1j * np.pi * hankel2(0.0, tmp) * R[a]
+
+#     # Do inverse fft on 0:dt:T and you have analytical solution
+#     U_t = 1.0/(2.0 * np.pi) * np.real(np.fft.ifft(U_a[:], nt))
+
+#     # The analytic solution needs be scaled by dx^2 to convert to pressure
+#     return np.real(U_t) * (model.spacing[0]**2)
+
+t_initial = 2.0
+dt = 0.01
+t_final = 2.0
+f0 = 5.0
+
+r = ricker(t_initial, t_final, dt, f0)
+
+# U_t = analytical(t_initial, dt, t_final, f0)
+# U_t = U_t[0:1501]
+
+print('END')
