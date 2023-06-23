da629ec7459a0e561a28bc798be47b4e01459f4a
Alexandre Olender <olender@usp.br>
2023-06-23 09:03:28 -0300

    propagation file

---
+++ dof_node_propagation.py
@@ -0,0 +1,100 @@
+import spyro
+from firedrake import RectangleMesh, conditional, UnitSquareMesh, Function, FunctionSpace, File
+import firedrake as fire
+import numpy as np
+from spyro.io.model_parameters import Model_parameters
+import matplotlib.pyplot as plt
+import sys
+
+
+dt = float(sys.argv[1])
+# dt = 0.0005
+final_time = 1.0
+# dx = 0.006546536707079771
+dx = 0.0
+
+dictionary = {}
+dictionary["options"] = {
+    "cell_type": "Q",  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
+    "variant": 'lumped',  # lumped, equispaced or DG, default is lumped "method":"MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
+    "degree": 4,  # p order
+    "dimension": 2,  # dimension
+}
+
+# Number of cores for the shot. For simplicity, we keep things serial.
+# spyro however supports both spatial parallelism and "shot" parallelism.
+dictionary["parallelism"] = {
+    "type": "automatic",  # options: automatic (same number of cores for evey processor) or spatial
+}
+
+# Define the domain size without the PML. Here we'll assume a 1.00 x 1.00 km
+# domain and reserve the remaining 250 m for the Perfectly Matched Layer (PML) to absorb
+# outgoing waves on three sides (eg., -z, +-x sides) of the domain.
+dictionary["mesh"] = {
+    "Lz": 3.0,  # depth in km - always positive
+    "Lx": 3.0,  # width in km - always positive
+    "Ly": 0.0,  # thickness in km - always positive
+    "mesh_file": None,
+    "user_mesh": None,
+}
+
+# Create a source injection operator. Here we use a single source with a
+# Ricker wavelet that has a peak frequency of 5 Hz injected at the center of the mesh.
+# We also specify to record the solution at a microphone near the top of the domain.
+# This transect of receivers is created with the helper function `create_transect`.
+dictionary["acquisition"] = {
+    "source_type": "ricker",
+    "source_locations": [(-1.5-dx, 1.5+dx)],#, (-0.605, 1.7), (-0.61, 1.7), (-0.615, 1.7)],#, (-0.1, 1.5), (-0.1, 2.0), (-0.1, 2.5), (-0.1, 3.0)],
+    "frequency": 5.0,
+    "delay": 1.5,
+    "receiver_locations": [(-1.5-dx, 2.0+dx)],
+}
+
+# Simulate for 2.0 seconds.
+dictionary["time_axis"] = {
+    "initial_time": 0.0,  # Initial time for event
+    "final_time": final_time,  # Final time for event
+    "dt": dt,  # timestep size
+    "amplitude": 1,  # the Ricker has an amplitude of 1.
+    "output_frequency": 100,  # how frequently to output solution to pvds
+    "gradient_sampling_frequency": 100,  # how frequently to save solution to RAM
+}
+
+dictionary["visualization"] = {
+    "forward_output" : True,
+    "output_filename": "results/forward_output.pvd",
+    "fwi_velocity_model_output": False,
+    "velocity_model_filename": None,
+    "gradient_output": False,
+    "gradient_filename": None,
+}
+
+Model = Model_parameters(dictionary=dictionary)
+
+Lx = 3
+Lz = 3
+user_mesh = fire.PeriodicRectangleMesh(150, 150, Lz, Lx, quadrilateral=True)
+user_mesh.coordinates.dat.data[:, 0] *= -1.0
+z, x = fire.SpatialCoordinate(user_mesh)
+
+cond = fire.conditional(x < 0.5, 1.5, 1.5)
+        
+
+Model.set_mesh(user_mesh=user_mesh)
+
+Wave_obj = spyro.AcousticWave(model_parameters=Model)
+
+Wave_obj.set_initial_velocity_model(conditional=cond)
+Wave_obj.forward_solve()
+
+time = np.linspace(0.0, final_time, int(final_time/dt)+1)
+
+rec_out = Wave_obj.receivers_output
+np.save("dof_quads_rec_out"+str(dt)+".npy", rec_out)
+
+plt.plot(time, Wave_obj.receivers_output)
+plt.show()
+
+print("END")
+
+
