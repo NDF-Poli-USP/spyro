487faf28443db0262450eb0b5458611adc7b122d
Alexandre Olender <olender@usp.br>
2023-07-10 21:48:32 -0300

    fixed test

--- test/test_readmesh.py
+++ test/test_readmesh.py
@@ -6,24 +6,20 @@ import spyro
 """Read in an external mesh and interpolate velocity to it"""
 
 def test_readmesh2():
-    from .inputfiles.Model1_2d_CG import model
+    from .inputfiles.Model1_2d_CG import model as oldmodel
+    model = spyro.Wave(dictionary=oldmodel)
 
-    comm = spyro.utils.mpi_init(model)
-
-    mesh, V = spyro.io.read_mesh(model, comm)
-
-    vp = spyro.io.interpolate(model, mesh, V)
+    vp = spyro.io.interpolate(model, oldmodel["mesh"]["initmodel"], model.function_space)
 
     assert not np.isnan(np.min(vp.dat.data[:]))
 
 def test_readmesh3():
-    from .inputfiles.Model1_3d_CG import model
+    from .inputfiles.Model1_3d_CG import model as oldmodel
+    receivers = spyro.create_transect((-0.05, 0.3, 0.5), (-0.05, 0.9, 0.5), 3)
+    oldmodel["acquisition"]["receiver_locations"] = receivers
+    model = spyro.Wave(dictionary=oldmodel)
 
-    comm = spyro.utils.mpi_init(model)
-
-    mesh, V = spyro.io.read_mesh(model, comm)
-
-    vp = spyro.io.interpolate(model, mesh, V)
+    vp = spyro.io.interpolate(model, oldmodel["mesh"]["initmodel"], model.function_space)
 
     assert not np.isnan(np.min(vp.dat.data[:]))
 
