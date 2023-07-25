65ef35e46608f81dd5d33cf9fa372f1d38405e76
Alexandre Olender <olender@usp.br>
2023-07-25 10:51:32 -0300

    moving meshing functions

--- spyro/meshing/meshing_functions.py
+++ spyro/meshing/meshing_functions.py
@@ -1,6 +1,170 @@
 import firedrake as fire
 
 
+class AutomaticMesh:
+    def __init__(self, dimension=2, comm=None):
+        """
+        Parameters
+        ----------
+        dimension : int, optional
+            Dimension of the mesh. The default is 2.
+        comm : MPI communicator, optional
+            MPI communicator. The default is None.
+        """
+        self.dimension = dimension
+        self.length_z = None
+        self.length_x = None
+        self.length_y = None
+        self.dx = None
+        self.quadrilateral = False
+        self.periodic = False
+        self.comm = comm
+        self.mesh_type = 'firedrake_mesh'
+
+    def set_mesh_size(self, length_z=None, length_x=None, length_y=None):
+        """
+        Parameters
+        ----------
+        length_z : float, optional
+            Length of the domain in the z direction. The default is None.
+        length_x : float, optional
+            Length of the domain in the x direction. The default is None.
+        length_y : float, optional
+            Length of the domain in the y direction. The default is None.
+
+        Returns
+        -------
+        None
+        """
+        if length_z is not None:
+            self.length_z = length_z
+        if length_x is not None:
+            self.length_x = length_x
+        if length_y is not None:
+            self.length_y = length_y
+
+    def set_meshing_parameters(self, dx=None, cell_type=None, mesh_type=None):
+        """
+        Parameters
+        ----------
+        dx : float, optional
+            Mesh size. The default is None.
+        cell_type : str, optional
+            Type of the cell. The default is None.
+        mesh_type : str, optional
+            Type of the mesh. The default is None.
+
+        Returns
+        -------
+        None
+        """
+        if cell_type is not None:
+            self.cell_type = cell_type
+        if self.cell_type == "quadrilateral":
+            self.quadrilateral = True
+        if dx is not None:
+            self.dx = dx
+        if mesh_type is not None:
+            self.mesh_type = mesh_type
+    
+    def make_periodic(self):
+        """
+        Sets the mesh boundaries periodic.
+        """
+        self.periodic = True
+        if self.mesh_type is not 'firedrake_mesh':
+            raise ValueError('periodic mesh is only supported for firedrake_mesh')
+    
+    def create_mesh(self):
+        """
+        Creates the mesh.
+
+        Returns
+        -------
+        mesh : Firedrake Mesh
+            Mesh
+        """
+        if self.dx is None and self.mesh_type == 'firedrake_mesh':
+            raise ValueError('dx is not set')
+        elif self.mesh_type == 'firedrake_mesh' and self.dimension == 2:
+            return self.create_firedrake_2D_mesh()
+        elif self.mesh_type == 'firedrake_mesh' and self.dimension == 3:
+            return self.create_firedrake_3D_mesh()
+        else:
+            raise ValueError('mesh_type is not supported')
+    
+    def create_firedrake_2D_mesh(self):
+        """
+        Creates a 2D mesh based on Firedrake meshing utilities.
+        """
+        nx = int(self.length_x / self.dx)
+        nz = int(self.length_z / self.dx)
+        comm = self.comm
+        if self.cell_type == "quadrilateral":
+            quadrilateral = True
+        else:
+            quadrilateral = False
+
+        if self.periodic:
+            return PeriodicRectangleMesh(
+                nz,
+                nx,
+                self.length_z,
+                self.length_x,
+                quadrilateral=quadrilateral,
+                comm=comm.comm,
+            )
+        else:
+            return RectangleMesh(
+                nz,
+                nx,
+                self.length_z,
+                self.length_x,
+                quadrilateral=quadrilateral,
+                comm=comm.comm,
+            )
+    
+    def create_firedrake_3D_mesh(self):
+        dx = self.dx
+        nx = int(self.length_x / dx)
+        nz = int(self.length_z / dx)
+        ny = int(self.length_y / dx)
+        if self.cell_type == "quadrilateral":
+            quadrilateral = True
+        else:
+            quadrilateral = False
+
+        return BoxMesh(
+            nz,
+            nx,
+            ny,
+            self.length_z,
+            self.length_x,
+            self.length_y,
+            quadrilateral=quadrilateral,
+        )
+
+
+# def create_firedrake_3D_mesh_based_on_parameters(dx, cell_type):
+#     nx = int(self.length_x / dx)
+#     nz = int(self.length_z / dx)
+#     ny = int(self.length_y / dx)
+#     if self.cell_type == "quadrilateral":
+#         quadrilateral = True
+#     else:
+#         quadrilateral = False
+
+#     return spyro.BoxMesh(
+#         nz,
+#         nx,
+#         ny,
+#         self.length_z,
+#         self.length_x,
+#         self.length_y,
+#         quadrilateral=quadrilateral,
+#     )
+
+
 def RectangleMesh(nx, ny, Lx, Ly, pad=None, comm=None, quadrilateral=False):
     """Create a rectangle mesh based on the Firedrake mesh.
     First axis is negative, second axis is positive. If there is a pad, both
