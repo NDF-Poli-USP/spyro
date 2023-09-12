0fb83861a1773f797e7c2069723fd34d06d3dec4
Alexandre Olender <olender@usp.br>
2023-09-12 13:03:10 -0300

    setting up radius calc

--- temp_mesh.py
+++ temp_mesh.py
@@ -3,6 +3,19 @@ import firedrake as fire
 import numpy as np
 
 
+def circle_circunscribe(T):
+    (x1, y1), (x2, y2), (x3, y3) = T
+    A = np.array([[x3-x1,y3-y1],[x3-x2,y3-y2]])
+    Y = np.array([(x3**2 + y3**2 - x1**2 - y1**2),(x3**2+y3**2 - x2**2-y2**2)])
+    if np.linalg.det(A) == 0:
+        return False
+    Ainv = np.linalg.inv(A)
+    X = 0.5*np.dot(Ainv,Y)
+    x,y = X[0],X[1]
+    r = np.sqrt((x-x1)**2+(y-y1)**2)
+    return (x,y),r
+
+
 Lz = 1.0
 Lx = 2.0
 c = 1.5
@@ -36,5 +49,9 @@ test1 = test1 and (np.isclose(np.amax(z), 0.0))
 test1 = test1 and (np.isclose(np.amin(x), -pad))
 
 # Checking cell diameter of an interior cell
+node_ids = V.cell_node_list[300]
+p0 = (z[node_ids[0]], x[node_ids[0]])
+p1 = (z[node_ids[1]], x[node_ids[1]])
+p2 = (z[node_ids[2]], x[node_ids[2]])
 
 print("end")
