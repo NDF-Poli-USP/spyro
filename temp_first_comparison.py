7c4d8f16350515c70bafb911fea756c2257d146e
Alexandre Olender <olender@usp.br>
2023-09-17 17:55:23 -0300

    trying first case

---
+++ temp_first_comparison.py
@@ -0,0 +1,40 @@
+import spyro
+
+grid_point_calculator_parameters = {
+    # Experiment parameters
+    # Here we define the frequency of the Ricker wavelet source
+    "source_frequency": 5.0,
+    # The minimum velocity present in the domain.
+    "minimum_velocity_in_the_domain": 1.5,
+    # if an homogeneous test case is used this velocity will be defined in
+    # the whole domain.
+    # Either or heterogeneous. If heterogeneous is
+    "velocity_profile_type": "homogeneous",
+    # chosen be careful to have the desired velocity model below.
+    "velocity_model_file_name": None,
+    # FEM to evaluate such as `KMV` or `spectral`
+    # (GLL nodes on quads and hexas)
+    "FEM_method_to_evaluate": "mass_lumped_triangle",
+    "dimension": 2,  # Domain dimension. Either 2 or 3.
+    # Either near or line. Near defines a receiver grid near to the source,
+    "receiver_setup": "near",
+    # line defines a line of point receivers with pre-established near and far
+    # offsets.
+    # Line search parameters
+    "load_reference": False,
+    "save_reference": True,
+    "reference_degree": None,  # Degree to use in the reference case (int)
+    # grid point density to use in the reference case (float)
+    "C_reference": None,
+    "desired_degree": 4,  # degree we are calculating G for. (int)
+    "C_initial": 6.0,  # Initial G for line search (float)
+    "accepted_error_threshold": 0.05,
+    "C_accuracy": 1e-1,
+}
+
+Cpw_calc = spyro.tools.Meshing_parameter_calculator(grid_point_calculator_parameters)
+Wave_obj = Cpw_calc.initial_guess_object
+spyro.io.load_shots(Wave_obj, file_name="test_shot_record"+str(6.6))
+rec_num = Wave_obj.forward_solution_receivers
+rec_ana = Cpw_calc.reference_solution
+print("END")
