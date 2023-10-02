fb438d94f175251470db9adc8e36a9df11936954
Alexandre Olender <olender@usp.br>
2023-10-02 10:58:35 -0300

    cleaning up repeated sections

--- spyro/tools/cells_per_wavelength_calculator.py
+++ spyro/tools/cells_per_wavelength_calculator.py
@@ -118,15 +118,10 @@ class Meshing_parameter_calculator:
         print("Starting line search", flush=True)
 
         fast_loop = True
+        dif = 0.0
         cont = 0
         while error > TOL:
-            if fast_loop:
-                dif = max(0.1 * cpw, accuracy)
-            else:
-                dif = accuracy
 
-            if error != 100.0:
-                cpw = cpw + dif
             print("Trying cells-per-wavelength = ", cpw, flush=True)
 
             # Running forward model
@@ -136,7 +131,7 @@ class Meshing_parameter_calculator:
             p_receivers = Wave_obj.forward_solution_receivers
             spyro.io.save_shots(Wave_obj, file_name="test_shot_record"+str(cpw))
 
-            error = new_error_calc(p_receivers, self.reference_solution, Wave_obj.dt)
+            error = error_calc(p_receivers, self.reference_solution, Wave_obj.dt)
             print("Error is ", error, flush=True)
 
             if cont == 0 and error < TOL:
@@ -147,6 +142,9 @@ class Meshing_parameter_calculator:
                 # Flooring CPW to the neartest decimal point inside accuracy
                 cpw = np.round((cpw+1e-6) // accuracy * accuracy, int(-np.log10(accuracy)))
                 fast_loop = False
+            else:
+                dif = calculate_dif(cpw, accuracy, fast_loop=fast_loop)
+                cpw += dif
 
             cont += 1
 
@@ -164,7 +162,16 @@ class Meshing_parameter_calculator:
         return Wave_obj
 
 
-def new_error_calc(receivers, analytical, dt):
+def calculate_dif(cpw, accuracy, fast_loop=False):
+    if fast_loop:
+        dif = max(0.1 * cpw, accuracy)
+    else:
+        dif = accuracy
+
+    return dif
+
+
+def error_calc(receivers, analytical, dt):
     rec_len, num_rec = np.shape(receivers)
 
     # Interpolate analytical solution into numerical dts
@@ -190,182 +197,3 @@ def new_error_calc(receivers, analytical, dt):
 
     error = np.sqrt(squared_error)
     return error
-
-
-def error_calc(p_exact, p, model, comm=False):
-    """Calculates the error between the exact and the numerical solution
-
-    Parameters
-    ----------
-    p_exact : `firedrake.Function`
-        The exact pressure field
-    p : `firedrake.Function`
-        The numerical pressure field
-    model : `dictionary`
-        Contains simulation parameters and options.
-    comm : Firedrake.ensemble_communicator, optional
-        An ensemble communicator
-
-    Returns
-    -------
-    error : `float`
-        The error between the exact and the numerical solution
-
-    """
-    # p0 doesn't necessarily have the same dt as p_exact
-    # therefore we have to interpolate the missing points
-    # to have them at the same length
-    # testing shape
-    times_p_exact, _ = p_exact.shape
-    times_p, _ = p.shape
-    if times_p_exact > times_p:  # then we interpolate p_exact
-        times, receivers = p.shape
-        dt = model["time_axis"]["final_time"] / times
-        p_exact = time_interpolation(p_exact, p, model)
-    elif times_p_exact < times_p:  # then we interpolate p
-        times, receivers = p_exact.shape
-        dt = model["time_axis"]["final_time"] / times
-        p = time_interpolation(p, p_exact, model)
-    else:  # then we dont need to interpolate
-        times, receivers = p.shape
-        dt = model["time_axis"]["final_time"] / times
-    # p = time_interpolation(p, p_exact, model)
-
-    max_absolute_diff = 0.0
-    max_percentage_diff = 0.0
-
-    if comm.ensemble_comm.rank == 0:
-        numerator = 0.0
-        denominator = 0.0
-        for receiver in range(receivers):
-            numerator_time_int = 0.0
-            denominator_time_int = 0.0
-            for t in range(times - 1):
-                top_integration = (
-                    p_exact[t, receiver] - p[t, receiver]
-                ) ** 2 * dt
-                bot_integration = (p_exact[t, receiver]) ** 2 * dt
-
-                # Adding 1e-25 filter to receivers to eliminate noise
-                numerator_time_int += top_integration
-
-                denominator_time_int += bot_integration
-
-                diff = p_exact[t, receiver] - p[t, receiver]
-                if abs(diff) > 1e-15 and abs(diff) > max_absolute_diff:
-                    max_absolute_diff = copy.deepcopy(diff)
-
-                if abs(diff) > 1e-15 and abs(p_exact[t, receiver]) > 1e-15:
-                    percentage_diff = abs(diff / p_exact[t, receiver]) * 100
-                    if percentage_diff > max_percentage_diff:
-                        max_percentage_diff = copy.deepcopy(percentage_diff)
-
-            numerator += numerator_time_int
-            denominator += denominator_time_int
-
-    if denominator > 1e-15:
-        error = np.sqrt(numerator / denominator)
-
-    # if numerator < 1e-15:
-    #     print('Warning: error too small to measure correctly.', flush = True)
-    #     error = 0.0
-    if denominator < 1e-15:
-        print("Warning: receivers don't appear to register a shot.", flush=True)
-        error = 0.0
-
-    # print("ERROR IS ", flush = True)
-    # print(error, flush = True)
-    # print("Maximum absolute error ", flush = True)
-    # print(max_absolute_diff, flush = True)
-    # print("Maximum percentage error ", flush = True)
-    # print(max_percentage_diff, flush = True)
-    return error
-
-
-def error_calc_line(p_exact, p, model, comm=False):
-    # p0 doesn't necessarily have the same dt as p_exact
-    # therefore we have to interpolate the missing points
-    # to have them at the same length
-    # testing shape
-    (times_p_exact,) = p_exact.shape
-    (times_p,) = p.shape
-    if times_p_exact > times_p:  # then we interpolate p_exact
-        (times,) = p.shape
-        dt = model["time_axis"]["final_time"] / times
-        p_exact = time_interpolation_line(p_exact, p, model)
-    elif times_p_exact < times_p:  # then we interpolate p
-        (times,) = p_exact.shape
-        dt = model["time_axis"]["final_time"] / times
-        p = time_interpolation_line(p, p_exact, model)
-    else:  # then we dont need to interpolate
-        (times,) = p.shape
-        dt = model["time_axis"]["final_time"] / times
-
-    if comm.ensemble_comm.rank == 0:
-        numerator_time_int = 0.0
-        denominator_time_int = 0.0
-        # Integrating with trapezoidal rule
-        for t in range(times - 1):
-            numerator_time_int += (p_exact[t] - p[t]) ** 2
-            denominator_time_int += (p_exact[t]) ** 2
-        numerator_time_int -= (
-            (p_exact[0] - p[0]) ** 2 + (p_exact[times - 1] - p[times - 1]) ** 2
-        ) / 2
-        numerator_time_int *= dt
-        denominator_time_int -= (p_exact[0] ** 2 + p_exact[times - 1] ** 2) / 2
-        denominator_time_int *= dt
-
-        # if denominator_time_int > 1e-15:
-        error = np.sqrt(numerator_time_int / denominator_time_int)
-
-        if denominator_time_int < 1e-15:
-            print(
-                "Warning: receivers don't appear to register a shot.",
-                flush=True,
-            )
-            error = 0.0
-
-    return error
-
-
-def time_interpolation(p_old, p_exact, model):
-    times, receivers = p_exact.shape
-    dt = model["time_axis"]["final_time"] / times
-
-    times_old, rec = p_old.shape
-    dt_old = model["time_axis"]["final_time"] / times_old
-    time_vector_old = np.zeros((1, times_old))
-    for ite in range(times_old):
-        time_vector_old[0, ite] = dt_old * ite
-
-    time_vector_new = np.zeros((1, times))
-    for ite in range(times):
-        time_vector_new[0, ite] = dt * ite
-
-    p = np.zeros((times, receivers))
-    for receiver in range(receivers):
-        f = interpolate.interp1d(time_vector_old[0, :], p_old[:, receiver])
-        p[:, receiver] = f(time_vector_new[0, :])
-
-    return p
-
-
-def time_interpolation_line(p_old, p_exact, model):
-    (times,) = p_exact.shape
-    dt = model["time_axis"]["final_time"] / times
-
-    (times_old,) = p_old.shape
-    dt_old = model["time_axis"]["final_time"] / times_old
-    time_vector_old = np.zeros((1, times_old))
-    for ite in range(times_old):
-        time_vector_old[0, ite] = dt_old * ite
-
-    time_vector_new = np.zeros((1, times))
-    for ite in range(times):
-        time_vector_new[0, ite] = dt * ite
-
-    p = np.zeros((times,))
-    f = interpolate.interp1d(time_vector_old[0, :], p_old[:])
-    p[:] = f(time_vector_new[0, :])
-
-    return p
