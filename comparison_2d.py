02e18ca2091c2b0712c87ffdbb505731cc6f0e82
olender <olender@usp.br>
2023-11-16 15:35:01 -0300

    Adding next ricker propagation 2d figures

--- comparison_2d.py
+++ comparison_2d.py
@@ -19,19 +19,19 @@ for dt in dts:
     p_numerical = np.load(numerical_filename)
     time = np.linspace(0.0, 1.0, int(1.0/dt)+1)
     nt = len(time)
-    a = np.shape(p_analytical)
-    print(f"P analytical shape {a}")
-    a = np.shape(p_numerical)
-    print(f"P numerical shape {a}")
     error_time = np.linalg.norm(p_analytical - p_numerical, 2) / np.sqrt(nt)
     errors.append(error_time)
     print(error_time)
 
-plt.loglog(dts, errors)
+plt.loglog(dts, errors, '-o', label="Numerical interior propagation")
 
 theory = [t**2 for t in dts]
 theory = [errors[0]*th/theory[0] for th in theory]
 
-plt.loglog(dts, theory, '--')
-
+plt.loglog(dts, theory, '--^', label="theoretical 2nd order in time")
+plt.legend()
+plt.title("Ricker interior point propagation convergence in time")
+plt.xlabel("dt [s]")
+plt.ylabel("error")
+plt.savefig("mls_ricker_2d_propagation_verification_figB.png")
 plt.show()
