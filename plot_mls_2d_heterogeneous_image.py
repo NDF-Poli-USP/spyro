0b1b27a59a3768790d17682feed9966a84a1eae9
olender <olender@usp.br>
2023-11-27 10:26:56 -0300

    heterogeneous data

---
+++ plot_mls_2d_heterogeneous_image.py
@@ -0,0 +1,173 @@
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+import matplotlib
+import copy
+
+
+def line_it(cs, error, ax, color, op=-1):
+    coeffs = np.polyfit(np.array(cs), np.log(np.array(error)), 1)
+    x = [cs[0], cs[-1]]
+    y = [0.0, 0.0]
+    y[0] = coeffs[0]*x[0]+coeffs[1]
+    y[1] = coeffs[0]*x[1]+coeffs[1]
+    # ax.plot(x,np.exp(y),'--', color = color)
+    if op == -1:
+        text_location = (cs[-1]-0.4, error[-1]*0.5)
+    elif op == 0:
+        text_location = (cs[-1]-0.85, error[-1]*0.6)
+    elif op == 1:
+        text_location = (cs[-1]-0.4, error[-1]*0.6)
+    elif op == 2:
+        text_location = (cs[-1]-0.4, error[-1]*0.6)
+    elif op == 3:
+        text_location = (cs[-1]-0.3, error[-1]*0.5)
+    elif op == 4:
+        text_location = (cs[-1]-0.8, error[-1]*0.5)
+    #ax.annotate(str(round(coeffs[0],2)), xy = text_location, color = color 
+
+
+def get_minumum_cpw_indice(cpws, errs):
+    errs_array = np.array(errs)
+    cpws_array = np.array(cpws)
+    acceptable_indices = np.where(errs_array < 0.05)
+    found_any = np.any(acceptable_indices)
+    if found_any:
+        acceptable_cpws = cpws_array[np.where(errs_array < 0.05)]
+        min_cpw = np.min(acceptable_cpws)
+
+        indice_array = np.where(cpws_array == min_cpw)
+
+        min_indice = indice_array[0][0]
+        return min_indice
+    else:
+        return np.nan
+
+
+# Load the pickle file
+with open('ml_results.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+ml2t = data["ml2t"]
+ml3t = data["ml3t"]
+ml4t = data["ml4t"]
+ml5t = data["ml5t"]
+
+plt.rcParams["font.family"] = "Times New Roman"
+
+cmap0 = matplotlib.cm.get_cmap("Oranges")
+cmap1 = matplotlib.cm.get_cmap("Blues")
+cmap2 = matplotlib.cm.get_cmap("Reds")
+cmap3 = matplotlib.cm.get_cmap("Purples")
+cmap4 = matplotlib.cm.get_cmap("Greys")
+
+colors_regular = [cmap0(0.2), cmap1(0.3), cmap2(0.5), cmap3(0.7), cmap4(0.9)]
+colors_dark = [cmap0(0.3), cmap1(0.4), cmap2(0.6), cmap3(0.8), cmap4(1.0)]
+
+fig, ax = plt.subplots(1, 2)
+leters_size = 20
+y_limits = (1, 200)
+alphas = [1.7326282059345566, 2.5500981353665586, 3.31722783359841, 4.637348434180895]
+
+p = [2, 3, 4, 5]
+cpwl = [
+    ml2t['c'].tolist(),
+    ml3t['c'].tolist(),
+    ml4t['c'].tolist(),
+    ml5t['c'].tolist(),
+]
+
+err = [
+    ml2t['error'].tolist(),
+    ml3t['error'].tolist(),
+    ml4t['error'].tolist(),
+    ml5t['error'].tolist(),
+]
+
+dts = [
+    ml2t['dt'].tolist(),
+    ml3t['dt'].tolist(),
+    ml4t['dt'].tolist(),
+    ml5t['dt'].tolist(),
+]
+
+runtimes = [
+    ml2t['runtime'].tolist(),
+    ml3t['runtime'].tolist(),
+    ml4t['runtime'].tolist(),
+    ml5t['runtime'].tolist(),
+]
+
+cont = 0
+for degree in p:
+    m = cpwl[cont]
+
+    error_percent = [i * 100 for i in err[cont]]
+
+    ax[0].plot(m, error_percent, "o", label="MLT" + str(degree)+"tri", color=colors_dark[cont])
+    cont += 1
+
+
+ax[0].plot([0.5, 12.0], [5, 5], 'k--')
+# ax[0] .set(xlabel = "Grid-points-per-wavelength (G)", ylabel = "E %")
+ax[0].set_title("Error with varying C")
+ax[0].set(xlabel="Cells-per-wavelength (C)", ylabel="E %")
+ax[0].set_yticks([3, 5, 10, 30, 100])
+ax[0].set_xticks([1, 2, 4, 6, 8, 10, 12])
+ax[0].set_xlim((0.0, 12.5))
+ax[0].set_ylim(y_limits)
+ax[0].set_yscale("log")
+ax[0].legend()
+ax[0].set_yticks([3, 5, 10, 30, 100])
+ax[0].yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax[0].text(-0.1, 1.0, '(a)', transform=ax[0].transAxes, size=leters_size)
+ax[0].yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+
+cont = 0
+for degree in p:
+    g = copy.deepcopy(cpwl[cont])
+    for ite in range(len(cpwl[cont])):
+        g[ite] = g[ite]*alphas[cont]
+
+    error_percent = [i * 100 for i in err[cont]]
+
+    ax[1].plot(g, error_percent, "o", label="MLT" + str(degree)+"tri", color=colors_dark[cont])
+    # line_it(g, error_percent, ax[1], color=colors_regular[cont], op=cont)
+    cont += 1
+
+ax[1].plot([3.0, 13.0], [5, 5], 'k--')
+ax[1].set_title("Error with varying G")
+ax[1].set(xlabel="Grid-points-per-wavelength (G)", ylabel="E %")
+ax[1].set_xticks([2, 4, 6, 8, 10, 12])
+ax[1].set_xlim((1.5, 12.5))
+ax[1].set_ylim(y_limits)
+
+ax[1].set_yscale("log")
+ax[1].set_yticks([3, 5, 10, 30, 100])
+ax[1].yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax[1].text(-0.1, 1.0, '(b)', transform=ax[1].transAxes, size=leters_size)
+
+fig.set_size_inches(9, 5)
+
+print("Table results:")
+print("Element \tminimum C \tminimum G \ttime-step (s) \t runtime (s)")
+print("------------------------------------------")
+for i in range(len(p)):
+    p_i = p[i]
+    cpw_i = cpwl[i]
+    err_i = err[i]
+    dts_i = dts[i]
+    runtimes_i = runtimes[i]
+    indice = get_minumum_cpw_indice(cpw_i, err_i)
+
+    g = copy.deepcopy(cpw_i)
+    for ite in range(len(cpw_i)):
+        g[ite] = g[ite]*alphas[i]
+    if np.isnan(indice):
+        print("ML" + str(p_i) + "tri \t\t" + "DNF" + "\t\t" + "DNF")
+    else:
+        # print(f"ML{p_i}tri \t\t{cpw_i[indice]:.2f}\t\t{g[indice]:.2f}")
+        print(f"ML{p_i}tri & ${cpw_i[indice]:.2f}$ & ${g[indice]:.2f}$ & ${dts_i[indice]:.2e}$ & ${runtimes_i[indice]:.1f}$ \\\\")
+
+fig.savefig("mls_cpw_results_homogeneous_mltriangles.png")
+plt.show()
---
+++ run_2d_het_marmousi.py
@@ -0,0 +1,49 @@
+import numpy as np
+import spyro
+
+
+def test_cpw_calc():
+    grid_point_calculator_parameters = {
+        # Experiment parameters
+        # Here we define the frequency of the Ricker wavelet source
+        "source_frequency": 5.0,
+        # The minimum velocity present in the domain.
+        "minimum_velocity_in_the_domain": 1.5,
+        # if an homogeneous test case is used this velocity will be defined in
+        # the whole domain.
+        # Either or heterogeneous. If heterogeneous is
+        "velocity_profile_type": "heterogeneous",
+        # chosen be careful to have the desired velocity model below.
+        "velocity_model_file_name": "/media/alexandre/Extreme SSD/common_files/velocity_models/vp_marmousi-ii.segy",
+        "length_z": 3.5,
+        "length_x": 17.0,
+        # FEM to evaluate such as `KMV` or `spectral`
+        # (GLL nodes on quads and hexas)
+        "FEM_method_to_evaluate": "mass_lumped_triangle",
+        "dimension": 2,  # Domain dimension. Either 2 or 3.
+        # Either near or line. Near defines a receiver grid near to the source,
+        "receiver_setup": "line",
+        # line defines a line of point receivers with pre-established near and far
+        # offsets.
+        # Line search parameters
+        "load_reference": True,
+        "save_reference": False,
+        "reference_degree": 5,  # Degree to use in the reference case (int)
+        # grid point density to use in the reference case (float)
+        "C_reference": 5.0,
+        "desired_degree": 4,  # degree we are calculating G for. (int)
+        "C_initial": 2.19,  # Initial G for line search (float)
+        "accepted_error_threshold": 0.05,
+        "C_accuracy": 0.01,
+    }
+
+    Cpw_calc = spyro.tools.Meshing_parameter_calculator(
+        grid_point_calculator_parameters
+    )
+    # Check if cpw is within error TOL, starting search at min
+    min = Cpw_calc.find_minimum(savetxt=True)
+    print(min, flush=True)
+
+
+if __name__ == "__main__":
+    test_cpw_calc()
