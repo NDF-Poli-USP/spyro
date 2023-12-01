a0cf1df4a4f4b589827b49c1efcbfa006a78d69f
olender <olender@usp.br>
2023-12-01 09:17:54 -0300

    adding updated tests

--- test/test_eikonal_fig18.py
+++ test/test_eikonal_fig18.py
@@ -1,7 +1,6 @@
 import spyro
-from spyro.habc import HABC
 import math
-from .generate_velocity_model_from_paper import get_paper_velocity
+from generate_velocity_model_from_paper import get_paper_velocity
 
 
 def test_eikonal_values_fig18():
@@ -9,7 +8,7 @@ def test_eikonal_values_fig18():
     dictionary["options"] = {
         "cell_type": "T",  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
         "variant": 'lumped',  # lumped, equispaced or DG, default is lumped "method":"MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
-        "degree": 1,  # p order
+        "degree": 4,  # p order
         "dimension": 2,  # dimension
     }
 
@@ -23,12 +22,12 @@ def test_eikonal_values_fig18():
     # domain and reserve the remaining 250 m for the Perfectly Matched Layer (PML) to absorb
     # outgoing waves on three sides (eg., -z, +-x sides) of the domain.
     dictionary["mesh"] = {
-        "Lz": 4.8,  # depth in km - always positive
-        "Lx": 2.4,  # width in km - always positive
+        "Lz": 2.4,  # depth in km - always positive
+        "Lx": 4.8,  # width in km - always positive
         "Ly": 0.0,  # thickness in km - always positive
         "mesh_file": None,
         "user_mesh": None,
-        "mesh_type": "firedrake_mesh",
+        "mesh_type": "SeismicMesh",
     }
 
     # Create a source injection operator. Here we use a single source with a
@@ -48,8 +47,8 @@ def test_eikonal_values_fig18():
     # Simulate for 2.0 seconds.
     dictionary["time_axis"] = {
         "initial_time": 0.0,  # Initial time for event
-        "final_time": 2.00,  # Final time for event
-        "dt": 0.001,  # timestep size
+        "final_time": 1.00,  # Final time for event
+        "dt": 0.0005,  # timestep size
         "amplitude": 1,  # the Ricker has an amplitude of 1.
         "output_frequency": 100,  # how frequently to output solution to pvds
         "gradient_sampling_frequency": 100,  # how frequently to save solution to RAM
@@ -57,44 +56,60 @@ def test_eikonal_values_fig18():
 
     dictionary["visualization"] = {
         "forward_output": True,
-        "output_filename": "results/forward_output.pvd",
+        "forward_output_filename": "results/figeigteen_forward_output.pvd",
         "fwi_velocity_model_output": False,
         "velocity_model_filename": None,
         "gradient_output": False,
         "gradient_filename": None,
+        "debug_output": True,
     }
 
-    Wave_no_habc = spyro.AcousticWave(dictionary=dictionary)
+    Wave_obj = spyro.AcousticWave(dictionary=dictionary)
 
-    Wave_no_habc.set_mesh(dx=0.01875)
-    V = Wave_no_habc.function_space
-    mesh = Wave_no_habc.mesh
+    cpw = 6.0
+    lba = 1.5 / 5.0
+    edge_length = lba / cpw
+    Wave_obj.set_mesh(mesh_parameters={"edge_length": edge_length})
+    V = Wave_obj.function_space
+    mesh = Wave_obj.mesh
     c = get_paper_velocity(mesh, V)
 
-    Wave_no_habc.set_initial_velocity_model(velocity_model_function=c)
-    Wave_no_habc._get_initial_velocity_model()
+    Wave_obj.set_initial_velocity_model(velocity_model_function=c)
+    Wave_obj._get_initial_velocity_model()
 
-    Wave_no_habc.c = Wave_no_habc.initial_velocity_model
+    Wave_obj.c = Wave_obj.initial_velocity_model
+    Wave_obj.get_and_set_maximum_dt(fraction=0.2)
+    Wave_obj.forward_solve()
 
-    habc = HABC(Wave_no_habc, h_min=0.01875)
+    min_value = Wave_obj.noneikonal_minimum
+    paper_min = 0.58755
 
-    eikonal = habc.eikonal
-
-    min_value = eikonal.min_value
-    max_value = eikonal.max_value
-
-    paper_min = 0.085
-    paper_max = 0.56
-
+    # Testing minimum values
     test_min = math.isclose(min_value, paper_min, rel_tol=0.1)
-    test_max = math.isclose(max_value, paper_max, rel_tol=0.2)
+    print("min_value: ", min_value)
+    print("paper_min: ", paper_min)
+    print(f"Passed the minimum value test: {test_min}")
 
-    assert all([test_min, test_max])
+    # Testing minimum location
+    z_min, x_min = Wave_obj.noneikonal_minimum_point
+    paper_z_min = -(2.4-1.925)
+    paper_x_min = 4.8
+
+    test_z_min = math.isclose(z_min, paper_z_min, rel_tol=0.1)
+    test_x_min = math.isclose(x_min, paper_x_min, rel_tol=0.1)
+
+    test_min_point = all([test_z_min, test_x_min])
+    print("z_min: ", z_min)
+    print("paper_z_min: ", paper_z_min)
+    print(f"Passed the minimum point location test: {test_min_point}")
+
+    assert all([test_min, test_min_point])
 
 
 # Verificar valores das distancias como lref e velocidades
 if __name__ == "__main__":
-    test_eikonal_values_fig8()
+    test_eikonal_values_fig18()
+
 
 # xloc[m] #yloc[m] #c[km/s] #eik[ms] 
 
--- test/test_eikonal_fig8.py
+++ test/test_eikonal_fig8.py
@@ -1,5 +1,4 @@
 import spyro
-from spyro.habc import HABC
 import firedrake as fire
 import math
 
@@ -9,7 +8,7 @@ def test_eikonal_values_fig8():
     dictionary["options"] = {
         "cell_type": "T",  # simplexes such as triangles or tetrahedra (T) or quadrilaterals (Q)
         "variant": 'lumped',  # lumped, equispaced or DG, default is lumped "method":"MLT", # (MLT/spectral_quadrilateral/DG_triangle/DG_quadrilateral) You can either specify a cell_type+variant or a method
-        "degree": 1,  # p order
+        "degree": 4,  # p order
         "dimension": 2,  # dimension
     }
 
@@ -26,9 +25,7 @@ def test_eikonal_values_fig8():
         "Lz": 1.0,  # depth in km - always positive
         "Lx": 1.0,  # width in km - always positive
         "Ly": 0.0,  # thickness in km - always positive
-        "mesh_file": None,
-        "user_mesh": None,
-        "mesh_type": "firedrake_mesh",
+        "mesh_type": "SeismicMesh",
     }
 
     # Create a source injection operator. Here we use a single source with a
@@ -48,53 +45,62 @@ def test_eikonal_values_fig8():
     # Simulate for 2.0 seconds.
     dictionary["time_axis"] = {
         "initial_time": 0.0,  # Initial time for event
-        "final_time": 2.00,  # Final time for event
-        "dt": 0.001,  # timestep size
+        "final_time": 1.00,  # Final time for event
+        "dt": 0.0005,  # timestep size
         "amplitude": 1,  # the Ricker has an amplitude of 1.
         "output_frequency": 100,  # how frequently to output solution to pvds
         "gradient_sampling_frequency": 100,  # how frequently to save solution to RAM
     }
 
     dictionary["visualization"] = {
-        "forward_output" : True,
-        "output_filename": "results/forward_output.pvd",
+        "forward_output": True,
+        "forward_output_filename": "results/fd_forward_output.pvd",
         "fwi_velocity_model_output": False,
         "velocity_model_filename": None,
         "gradient_output": False,
         "gradient_filename": None,
     }
 
-    Wave_no_habc = spyro.AcousticWave(dictionary=dictionary)
+    Wave_obj = spyro.AcousticWave(dictionary=dictionary)
 
-    Lx = 1
-    Lz = 1
-    user_mesh = fire.RectangleMesh(80, 80, Lz, Lx, diagonal="crossed")
-    user_mesh.coordinates.dat.data[:, 0] *= -1.0
-    z, x = fire.SpatialCoordinate(user_mesh)
+    # Using SeismicMesh:
+    cpw = 5.0
+    lba = 1.5 / 5.0
+    edge_length = lba / cpw
+    Wave_obj.set_mesh(mesh_parameters={"edge_length": edge_length})
+    cond = fire.conditional(Wave_obj.mesh_x < 0.5, 3.0, 1.5)
 
-    cond = fire.conditional(x < 0.5, 3.0, 1.5)
+    # Rest of setup
+    Wave_obj.set_initial_velocity_model(conditional=cond)
+    Wave_obj._get_initial_velocity_model()
 
-    Wave_no_habc.set_mesh(user_mesh=user_mesh)
+    Wave_obj.c = Wave_obj.initial_velocity_model
+    Wave_obj.forward_solve()
 
-    Wave_no_habc.set_initial_velocity_model(conditional=cond)
-    Wave_no_habc._get_initial_velocity_model()
-
-    Wave_no_habc.c = Wave_no_habc.initial_velocity_model
-
-    habc = HABC(Wave_no_habc, h_min=0.0125)
-
-    eikonal = habc.eikonal
-
-    min_value = eikonal.min_value
-    max_value = eikonal.max_value
+    min_value = Wave_obj.noneikonal_minimum
 
     paper_min = 0.085
-    paper_max = 0.56
 
+    # Testing minimum values
     test_min = math.isclose(min_value, paper_min, rel_tol=0.1)
-    test_max = math.isclose(max_value, paper_max, rel_tol=0.2)
+    print("min_value: ", min_value)
+    print("paper_min: ", paper_min)
+    print(f"Passed the minimum value test: {test_min}")
 
-    assert all([test_min, test_max])
+    # Testing minimum location
+    z_min, x_min = Wave_obj.noneikonal_minimum_point
+    paper_z_min = -0.5
+    paper_x_min = 0.0
+
+    test_z_min = math.isclose(z_min, paper_z_min, rel_tol=0.1)
+    test_x_min = math.isclose(x_min, paper_x_min, rel_tol=0.1)
+
+    test_min_point = all([test_z_min, test_x_min])
+    print("z_min: ", z_min)
+    print("paper_z_min: ", paper_z_min)
+    print(f"Passed the minimum point location test: {test_min_point}")
+
+    assert all([test_min, test_min_point])
 
 
 # Verificar valores das distancias como lref e velocidades
