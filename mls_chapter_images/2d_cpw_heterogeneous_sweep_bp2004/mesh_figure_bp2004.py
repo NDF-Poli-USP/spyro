9eb476bcf338b46f5806cd2cecdb54663d3f715b
olender <olender@usp.br>
2023-12-08 10:56:22 -0300

    deleting chapter folders

--- mls_chapter_images/2d_cpw_heterogeneous_sweep_bp2004/mesh_figure_bp2004.py
+++
@@ -1,6 +0,0 @@
-import spyro
-
-mesh_file = "automatic_mesh.msh"
-title_str = "BP2004 mesh C = 2.41"
-
-spyro.plots.plot_mesh_sizes(mesh_file, title_str=title_str, show=False, output_filename="adapted_mesh_marmousi.png")
--- mls_chapter_images/2d_cpw_heterogeneous_sweep_marmousi/mesh_figure_marmousi.py
+++
@@ -1,6 +0,0 @@
-import spyro
-
-mesh_file = "automatic_mesh.msh"
-title_str = "Marmousi mesh C = 2.29"
-
-spyro.plots.plot_mesh_sizes(mesh_file, title_str=title_str, show=False, output_filename="adapted_mesh_marmousi.png")
