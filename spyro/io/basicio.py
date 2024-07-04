ff13956596310c78314e61e21c0fbc8d36340017
olender <olender@usp.br>
2024-07-04 16:11:38 -0300

    added spatially parallelizable shots in serial

--- spyro/io/basicio.py
+++ spyro/io/basicio.py
@@ -67,7 +67,8 @@ def ensemble_propagator(func):
                 args[0].current_time = starting_time
                 u, u_r = func(*args, **dict(kwargs, source_nums=[snum]))
                 save_serial_data(args[0], snum)
-                return u, u_r
+                
+            return u, u_r
 
     return wrapper
 
@@ -75,15 +76,17 @@ def ensemble_propagator(func):
 def save_serial_data(wave, propagation_id):
     arrays_list = [obj.dat.data[:] for obj in wave.forward_solution]
     stacked_arrays = np.stack(arrays_list, axis=0)
-    np.save(f'tmp_shot{propagation_id}.npy', stacked_arrays)
-    np.save(f"tmp_rec{propagation_id}.npy", wave.forward_solution_receivers)
+    spatialcomm = wave.comm.comm.rank
+    np.save(f'tmp_shot{propagation_id}_comm{spatialcomm}.npy', stacked_arrays)
+    np.save(f"tmp_rec{propagation_id}_comm{spatialcomm}.npy", wave.forward_solution_receivers)
 
 
 def switch_serial_shot(wave, propagation_id):
-    stacked_shot_arrays = np.load(f'tmp_shot{propagation_id}.npy')
+    spatialcomm = wave.comm.comm.rank
+    stacked_shot_arrays = np.load(f'tmp_shot{propagation_id}_comm{spatialcomm}.npy')
     for array_i, array in enumerate(stacked_shot_arrays):
         wave.forward_solution[array_i].dat.data[:] = array
-    wave.forward_solution_receivers = np.load(f"tmp_rec{propagation_id}.npy")
+    wave.forward_solution_receivers = np.load(f"tmp_rec{propagation_id}_comm{spatialcomm}.npy")
 
 
 
