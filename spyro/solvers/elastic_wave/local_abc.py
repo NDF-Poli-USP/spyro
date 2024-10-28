b9be29efb8a45761b90302af9fdb26a0cb309d5f
Eduardo Moscatelli de Souza <5752216+SouzaEM@users.noreply.github.com>
2024-10-28 15:05:10 -0300

    Add Stacey boundary condition

--- spyro/solvers/elastic_wave/local_abc.py
+++ spyro/solvers/elastic_wave/local_abc.py
@@ -1,12 +1,20 @@
 from firedrake import (Constant, ds, TestFunction)
 
 
-def clayton_engquist_A1(wave):
+def local_abc_form(wave):
     '''
     Returns the linear form associated with the traction loads
-    when combined with the Clayton-Engquist A1 relations.
+    when combined with local absorbing boundary conditions.
     '''
-    F_t = 0  # linear form
+    abc_dict = wave.input_dictionary.get("absorving_boundary_conditions", None)
+    if abc_dict is None:
+        return 0
+    else:
+        abc_active = abc_dict.get("status", False)
+        if abc_active:
+            abc_type = abc_dict.get("local", {}).get("type", "Stacey")
+        else:
+            return 0
 
     V = wave.function_space
     v = TestFunction(V)
@@ -39,48 +47,78 @@ def clayton_engquist_A1(wave):
         uy_dz = u_n[iy].dx(iz)
         uy_dx = u_n[iy].dx(ix)
         uy_dy = u_n[iy].dx(iy)
+    else:
+        uy_dt = None
+        uz_dy = None
+        ux_dy = None
+        uy_dz = None
+        uy_dx = None
+        uy_dy = None
+
+    if abc_type == "Stacey":
+        callback = stacey_terms
+    elif abc_type == "CE_A1":
+        callback = clayton_engquist_A1_terms
+    else:
+        raise NotImplementedError(f"Unsupported local ABC: {abc_type}")
+
+    return callback(wave.dimension, rho, c_p, c_s,
+                    v, iz, ix, iy, qr_s,
+                    uz_dt, ux_dt, uy_dt,
+                    uz_dz, ux_dz, uy_dz,
+                    uz_dx, ux_dx, uy_dx,
+                    uz_dy, ux_dy, uy_dy)
+
+def clayton_engquist_A1_terms(ndim, rho, c_p, c_s,
+                              v, iz, ix, iy, qr_s,
+                              uz_dt, ux_dt, uy_dt,
+                              uz_dz, ux_dz, uy_dz,
+                              uz_dx, ux_dx, uy_dx,
+                              uz_dy, ux_dy, uy_dy):
+
+    F_t = 0
 
     # Plane z = -(Lz + pad)
     sig_zz = rho*c_p*uz_dt + rho*(c_p**2 - 2*c_s**2)*ux_dx
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_zz += rho*(c_p**2 - 2*c_s**2)*uy_dy
     sig_xz = rho*c_s*ux_dt + rho*(c_s**2)*uz_dx
     F_t += -(sig_zz*v[iz] + sig_xz*v[ix])*ds(1, scheme=qr_s)
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_yz = rho*c_s*uy_dt + rho*(c_s**2)*uz_dy
         F_t += -sig_yz*v[iy]*ds(1, scheme=qr_s)
 
     # Plane z = 0
     sig_zz = -rho*c_p*uz_dt + rho*(c_p**2 - 2*c_s**2)*ux_dx
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_zz += rho*(c_p**2 - 2*c_s**2)*uy_dy
     sig_xz = -rho*c_s*ux_dt + rho*(c_s**2)*uz_dx
     F_t += (sig_zz*v[iz] + sig_xz*v[ix])*ds(2, scheme=qr_s)
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_yz = -rho*c_s*uy_dt + rho*(c_s**2)*uz_dy
         F_t += sig_yz*v[iy]*ds(2, scheme=qr_s)
 
     # Plane x = -pad
     sig_zx = rho*c_s*uz_dt + rho*(c_s**2)*ux_dz
     sig_xx = rho*c_p*ux_dt + rho*(c_p**2 - 2*c_s**2)*uz_dz
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_xx += rho*(c_p**2 - 2*c_s**2)*uy_dy
     F_t += -(sig_zx*v[iz] + sig_xx*v[ix])*ds(3, scheme=qr_s)
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_yx = rho*c_s*uy_dt + rho*(c_s**2)*ux_dy
         F_t += -sig_yx*v[iy]*ds(3, scheme=qr_s)
 
     # Plane x = Lx + pad
     sig_zx = -rho*c_s*uz_dt + rho*(c_s**2)*ux_dz
     sig_xx = -rho*c_p*ux_dt + rho*(c_p**2 - 2*c_s**2)*uz_dz
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_xx += rho*(c_p**2 - 2*c_s**2)*uy_dy
     F_t += (sig_zx*v[iz] + sig_xx*v[ix])*ds(4, scheme=qr_s)
-    if wave.dimension == 3:
+    if ndim == 3:
         sig_yx = -rho*c_s*uy_dt + rho*(c_s**2)*ux_dy
         F_t += sig_yx*v[iy]*ds(4, scheme=qr_s)
 
-    if wave.dimension == 3:
+    if ndim == 3:
         # Plane y = 0
         sig_zy = rho*c_s*uz_dt + rho*(c_s**2)*uy_dz
         sig_xy = rho*c_s*ux_dt + rho*(c_s**2)*uy_dx
@@ -91,6 +129,70 @@ def clayton_engquist_A1(wave):
         sig_zy = -rho*c_s*uz_dt + rho*(c_s**2)*uy_dz
         sig_xy = -rho*c_s*ux_dt + rho*(c_s**2)*uy_dx
         sig_yy = -rho*c_p*uy_dt + rho*(c_p**2 - 2*c_s**2)*(uz_dz + ux_dx)
+        F_t += (sig_zy*v[iz] + sig_xy*v[ix] + sig_yy*v[iy])*ds(6, scheme=qr_s)
+
+    return F_t
+
+def stacey_terms(ndim, rho, c_p, c_s,
+                 v, iz, ix, iy, qr_s,
+                 uz_dt, ux_dt, uy_dt,
+                 uz_dz, ux_dz, uy_dz,
+                 uz_dx, ux_dx, uy_dx,
+                 uz_dy, ux_dy, uy_dy):
+
+    F_t = 0
+
+    # Plane z = -(Lz + pad)
+    sig_zz = rho*c_p*uz_dt + rho*c_s*(c_p - 2*c_s)*ux_dx
+    if ndim == 3:
+        sig_zz += rho*c_s*(c_p - 2*c_s)*uy_dy
+    sig_xz = rho*c_s*ux_dt + rho*c_s*(2*c_s - c_p)*uz_dx
+    F_t += -(sig_zz*v[iz] + sig_xz*v[ix])*ds(1, scheme=qr_s)
+    if ndim == 3:
+        sig_yz = rho*c_s*uy_dt + rho*c_s*(2*c_s - c_p)*uz_dy
+        F_t += -sig_yz*v[iy]*ds(1, scheme=qr_s)
+
+    # Plane z = 0
+    sig_zz = -rho*c_p*uz_dt + rho*c_s*(c_p - 2*c_s)*ux_dx
+    if ndim == 3:
+        sig_zz += rho*c_s*(c_p - 2*c_s)*uy_dy
+    sig_xz = -rho*c_s*ux_dt - rho*c_s*(c_p - 2*c_s)*uz_dx
+    F_t += (sig_zz*v[iz] + sig_xz*v[ix])*ds(2, scheme=qr_s)
+    if ndim == 3:
+        sig_yz = -rho*c_s*uy_dt - rho*c_s*(c_p - 2*c_s)*uz_dy
+        F_t += sig_yz*v[iy]*ds(2, scheme=qr_s)
+
+    # Plane x = -pad
+    sig_zx = rho*c_s*uz_dt - rho*c_s*(c_p - 2*c_s)*ux_dz
+    sig_xx = rho*c_p*ux_dt + rho*c_s*(c_p - 2*c_s)*uz_dz
+    if ndim == 3:
+        sig_xx += rho*c_s*(c_p - 2*c_s)*uy_dy
+    F_t += -(sig_zx*v[iz] + sig_xx*v[ix])*ds(3, scheme=qr_s)
+    if ndim == 3:
+        sig_yx = rho*c_s*uy_dt - rho*c_s*(c_p - 2*c_s)*ux_dy
+        F_t += -sig_yx*v[iy]*ds(3, scheme=qr_s)
+
+    # Plane x = Lx + pad
+    sig_zx = -rho*c_s*uz_dt - rho*c_s*(c_p - 2*c_s)*ux_dz
+    sig_xx = -rho*c_p*ux_dt + rho*c_s*(c_p - 2*c_s)*uz_dz
+    if ndim == 3:
+        sig_xx += rho*c_s*(c_p - 2*c_s)*uy_dy
+    F_t += -(sig_zx*v[iz] + sig_xx*v[ix])*ds(3, scheme=qr_s)
+    if ndim == 3:
+        sig_yx = -rho*c_s*uy_dt - rho*c_s*(c_p - 2*c_s)*ux_dy
+        F_t += -sig_yx*v[iy]*ds(3, scheme=qr_s)
+
+    if ndim == 3:
+        # Plane y = 0
+        sig_zy = rho*c_s*uz_dt - rho*c_s*(c_p - 2*c_s)*uy_dz
+        sig_xy = rho*c_s*ux_dt - rho*c_s*(c_p - 2*c_s)*uy_dx
+        sig_yy = rho*c_p*uy_dt + rho*c_s*(c_p - 2*c_s)*(uz_dz + ux_dx)
+        F_t += -(sig_zy*v[iz] + sig_xy*v[ix] + sig_yy*v[iy])*ds(5, scheme=qr_s)
+
+        # Plane y = L_y + 2*pad
+        sig_zy = -rho*c_s*uz_dt - rho*c_s*(c_p - 2*c_s)*uy_dz
+        sig_xy = -rho*c_s*ux_dt - rho*c_s*(c_p - 2*c_s)*uy_dx
+        sig_yy = -rho*c_p*uy_dt + rho*c_s*(c_p - 2*c_s)*(uz_dz + ux_dx)
         F_t += (sig_zy*v[iz] + sig_xy*v[ix] + sig_yy*v[iy])*ds(6, scheme=qr_s)
 
     return F_t
