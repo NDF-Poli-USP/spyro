f011fc7763877546ee0d26874931e701b0e5a628
olender <olender@usp.br>
2024-11-07 15:05:17 -0300

    updating license information

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="spyro",
-    version="0.2.0",
-    license="GPL v3",
-    description="acoustic wave modeling with the finite element method",
-    author="Keith J. Roberts, Alexandre F. G. Olender, Lucas Franceschini",
-    url="https://github.com/krober10nd/spyro",
+    version="0.9.0",
+    license="LGPL v3",
+    description="Wave modeling with the finite element method",
+    author="Keith J. Roberts, Alexandre F. G. Olender, Lucas Franceschini, Eduardo Moscatelli de Souza, Daiane I. Dolci",
+    url="https://github.com/NDF-Poli-USP/spyro",
     packages=find_packages(),
     install_requires=[
+        "firedrake",
         "numpy",
         "scipy",
         "matplotlib",
         "exdown==0.7.0",
-        "segyio"],
+        "segyio",
+        "meshio"],
 )
