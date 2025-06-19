2011b8f939b596f1e57b58b2debabd2bfc8b9e2d
Olender <olender@usp.br>
2025-06-19 18:24:34 -0300

    debugging tests

--- spyro/io/model_parameters.py
+++ spyro/io/model_parameters.py
@@ -315,6 +315,7 @@ class Model_parameters:
         self.input_dictionary["mesh"].setdefault("user_mesh", None)
         self.input_dictionary.setdefault("absorving_boundary_conditions", {})
         self.input_dictionary["absorving_boundary_conditions"].setdefault("pad_length", None)
+        self.input_dictionary["absorving_boundary_conditions"].setdefault("status", False)
         self.user_mesh = self.input_dictionary["mesh"]["user_mesh"]
         mesh_parameters = meshing.MeshingParameters(
             input_mesh_dictionary=self.input_dictionary["mesh"],
