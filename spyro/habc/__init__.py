4b3c7e74ec8a1467146f3b07924b224ab00d4d69
ruansava <ruansava@gmail.com>
2025-06-30 12:53:19 -0300

    Refactor dynamic module import in __init__.py
    Replaces deprecated loader-based dynamic import with importlib for importing submodules

--- spyro/habc/__init__.py
+++ spyro/habc/__init__.py
@@ -1,7 +1,9 @@
+import importlib
 import pkgutil
 
 __all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+for module_info in pkgutil.walk_packages(__path__):
+    module_name = module_info.name
     __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+    module = importlib.import_module(f"{__name__}.{module_name}")
+    globals()[module_name] = module
