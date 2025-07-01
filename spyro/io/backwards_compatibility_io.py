7b58bfcda3903a5a677559a5ee0ebabb6c26a59e
Olender <olender@usp.br>
2025-07-01 17:36:51 -0300

    fixing old dictionary

--- spyro/io/backwards_compatibility_io.py
+++ spyro/io/backwards_compatibility_io.py
@@ -83,6 +83,10 @@ class Dictionary_conversion:
         variant = self.new_dictionary["options"]["variant"]
         if variant == "GLL" or variant == "KMV":
             self.new_dictionary["options"]["variant"] = "lumped"
+        if variant == "KMV":
+            self.new_dictionary["options"]["method"] = "mass_lumped_triangle"
+        if variant == "GLL":
+            self.new_dictionary["options"]["method"] = "spectral_quadrilateral"
 
     def convert_parallelism(self):
         """
@@ -219,9 +223,13 @@ class Dictionary_conversion:
         convert the absorving_boundary_conditions section of dictionary
         """
         old_dictionary = self.old_dictionary["BCs"]
+        if old_dictionary["status"]:
+            damping_type = "PML"
+        else:
+            damping_type = None
         self.new_dictionary["absorving_boundary_conditions"] = {
             "status": old_dictionary["status"],
-            "damping_type": "PML",
+            "damping_type": damping_type,
             "exponent": old_dictionary["exponent"],
             "cmax": old_dictionary["cmax"],
             "R": old_dictionary["R"],
