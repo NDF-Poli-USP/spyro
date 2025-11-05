2a84955282f469b516900b4443010b634f3a9dde
Olender <olender@usp.br>
2025-11-05 22:05:40 +0000

    updating comments

--- tests/parallel/test_gradient_serialshots.py
+++ tests/parallel/test_gradient_serialshots.py
@@ -25,7 +25,7 @@ dictionary["parallelism"] = {
 }
 
 dictionary["mesh"] = {
-    "Lz": 3.0,  # depth in km - always positive   # Como ver isso sem ler a malha?
+    "Lz": 3.0,  # depth in km - always positive
     "Lx": 3.0,  # width in km - always positive
     "Ly": 0.0,  # thickness in km - always positive
     "mesh_file": None,
@@ -46,8 +46,8 @@ dictionary["time_axis"] = {
     "final_time": final_time,  # Final time for event
     "dt": 0.0005,  # timestep size
     "amplitude": 1,  # the Ricker has an amplitude of 1.
-    "output_frequency": 100,  # how frequently to output solution to pvds - Perguntar Daiane ''post_processing_frequnecy'
-    "gradient_sampling_frequency": 1,  # how frequently to save solution to RAM    - Perguntar Daiane 'gradient_sampling_frequency'
+    "output_frequency": 100,  # how frequently to output solution to pvds
+    "gradient_sampling_frequency": 1,  # how frequently to save solution to RAM
 }
 
 dictionary["visualization"] = {
