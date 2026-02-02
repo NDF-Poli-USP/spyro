c1d0a5dcc9b943fd1a3c323bf549be97263a697f
ruansava <ruansava@gmail.com>
2026-02-02 14:44:38 -0300

    Add PML sigma field and refactor Wave_obj naming
    Introduce calc_pml_damping and pml_sigma_field helpers to compute PML damping profiles, and use them in the 2D/3D solver constructors.

--- spyro/solvers/acoustic_solver_construction_with_pml.py
+++ spyro/solvers/acoustic_solver_construction_with_pml.py
@@ -8,11 +8,10 @@ from numpy import where
 #   "A Modified PML Acoustic Wave Equation". Kim (2019)
 
 
-def construct_solver_or_matrix_with_pml(Wave_object):
+def construct_solver_or_matrix_with_pml(Wave_obj):
     '''
     Build solver operators for wave propagator with a PML. Doesn't create
     mass matrices if matrix_free option is on, which it is by default.
-
 
     Parameters
     ----------
@@ -24,13 +23,142 @@ def construct_solver_or_matrix_with_pml(Wave_object):
     solver: Firedrake 'LinearSolver'
         Linear solver for the wave equation with PML
     '''
-    V = Wave_object.function_space
+    V = Wave_obj.function_space
     Z = fire.VectorFunctionSpace(V.ufl_domain(), V.ufl_element())
-    Wave_object.vector_function_space = Z
-    if Wave_object.dimension == 2:
-        return construct_solver_or_matrix_with_pml_2d(Wave_object)
-    elif Wave_object.dimension == 3:
-        return construct_solver_or_matrix_with_pml_3d(Wave_object)
+    Wave_obj.vector_function_space = Z
+    if Wave_obj.dimension == 2:
+        return construct_solver_or_matrix_with_pml_2d(Wave_obj)
+    elif Wave_obj.dimension == 3:
+        return construct_solver_or_matrix_with_pml_3d(Wave_obj)
+
+
+def calc_pml_damping(pad_len, dgr_prof=2, CR=0.001):
+    '''
+    Calculate the maximum damping coefficient for the PML layer.
+
+    Parameters
+    ----------
+    pad_len : `float`
+        Length of the PML layer
+    dgr_prof : `int`, optional
+        Degree of the damping profile within the PML layer.
+    CR : `float`, optional
+        Desired reflection coefficient at outer boundary of PML layer.
+        Default is 0.001
+
+    Returns
+    -------
+    sigma_max : `float`
+        Maximum damping coefficient within the PML layer
+    '''
+
+    sigma_max = (dgr_prof + 1.) / (2. * pad_len) * np.log(1 / CR)
+
+    return sigma_max
+
+
+def pml_sigma_field(Wave_obj):
+    '''
+    Generate a damping profile for the PML.
+
+    Parameters
+    ----------
+    coords : 'ufl.geometry.SpatialCoordinate'
+        Domain Coordinates including the absorbing layer
+    V : `firedrake function space`
+        Function space for the mask field
+    pad_len : `float`
+        Length of the PML layer
+    sigma_max : `float`
+        Maximum damping coefficient within the PML layer
+
+    Returns
+    -------
+    sigma_x : obj
+        Firedrake function with the damping function in the x direction
+    sigma_z : obj
+        Firedrake function with the damping function in the z direction
+    sigma_y : obj
+        Firedrake function with the damping function in the y direction
+    '''
+
+    ps = Wave_obj.abc_exponent
+    cmax = Wave_obj.abc_cmax  # maximum acoustic wave velocity
+    R = Wave_obj.abc_R  # theoretical reclection coefficient
+    pad_length = Wave_obj.abc_pad_length  # length of the padding
+    V = Wave_obj.function_space
+    dimension = Wave_obj.dimension
+    z = Wave_obj.mesh_z
+    x = Wave_obj.mesh_x
+    x1 = 0.0
+    x2 = Wave_obj.length_x
+    z2 = -Wave_obj.length_z
+
+    # Compute the maximum damping coefficient
+    bar_sigma = calc_pml_damping(abc_pad_length)
+
+    aux1 = Function(V)
+    aux2 = Function(V)
+
+    # Sigma X
+    sigma_max_x = bar_sigma  # Max damping
+    aux1.interpolate(
+        conditional(
+            And((x >= x1 - pad_length), x < x1),
+            ((abs(x - x1) ** (ps)) / (pad_length ** (ps))) * sigma_max_x,
+            0.0,
+        )
+    )
+    aux2.interpolate(
+        conditional(
+            And(x > x2, (x <= x2 + pad_length)),
+            ((abs(x - x2) ** (ps)) / (pad_length ** (ps))) * sigma_max_x,
+            0.0,
+        )
+    )
+    sigma_x = Function(V, name="sigma_x").interpolate(aux1 + aux2)
+
+    # Sigma Z
+    tol_z = 1.000001
+    sigma_max_z = bar_sigma  # Max damping
+    aux1.interpolate(
+        conditional(
+            And(z < z2, (z >= z2 - tol_z * pad_length)),
+            ((abs(z - z2) ** (ps)) / (pad_length ** (ps))) * sigma_max_z,
+            0.0,
+        )
+    )
+
+    sigma_z = Function(V, name="sigma_z").interpolate(aux1)
+
+    if dimension == 2:
+        return (sigma_x, sigma_z)
+
+    elif dimension == 3:
+        # Sigma Y
+        sigma_max_y = bar_sigma  # Max damping
+        y = Wave_obj.mesh_y
+        y1 = 0.0
+        y2 = Wave_obj.length_y
+        aux1.interpolate(
+            conditional(
+                And((y >= y1 - pad_length), y < y1),
+                ((abs(y - y1) ** (ps)) / (pad_length ** (ps))) * sigma_max_y,
+                0.0,
+            )
+        )
+        aux2.interpolate(
+            conditional(
+                And(y > y2, (y <= y2 + pad_length)),
+                ((abs(y - y2) ** (ps)) / (pad_length ** (ps))) * sigma_max_y,
+                0.0,
+            )
+        )
+        sigma_y = Function(V, name="sigma_y").interpolate(aux1 + aux2)
+        # sgm_y = VTKFile("pmlField/sigma_y.pvd")
+        # sgm_y.write(sigma_y)
+
+        return (sigma_x, sigma_y, sigma_z)
 
 
 def damping_pml_2d(sigma_z, sigma_x):
@@ -92,13 +220,13 @@ def damping_pml_3d(sigma_z, sigma_x, sigma_y):
     return Gamma_1, Gamma_2, Gamma_3
 
 
-def Dirichlet_bc_pml(Wave_object, W):
+def Dirichlet_bc_pml(Wave_obj, W):
     '''
     Apply Dirichlet boundary conditions to the PML boundaries.
 
     Parameters
     ----------
-    Wave_object : `habc.HABC_Wave`
+    Wave_obj : `habc.HABC_Wave`
         An instance of the HABC_Wave class
     W : Firedrake 'MixedFunctionSpace'
         Mixed function space for the wave equation with PML
@@ -109,12 +237,12 @@ def Dirichlet_bc_pml(Wave_object, W):
         Dirichlet boundary conditions applied to the PML boundaries
     '''
 
-    bnds = [Wave_object.absorb_top, Wave_object.absorb_bottom,
-            Wave_object.absorb_right, Wave_object.absorb_left]
+    bnds = [Wave_obj.absorb_top, Wave_obj.absorb_bottom,
+            Wave_obj.absorb_right, Wave_obj.absorb_left]
 
-    if Wave_object.dimension == 3:
-        bnds.extend([Wave_object.absorb_front,
-                     Wave_object.absorb_back])
+    if Wave_obj.dimension == 3:
+        bnds.extend([Wave_obj.absorb_front,
+                     Wave_obj.absorb_back])
 
     # Tuple of boundary ids for Dirichlet BC
     where_to_absorb = tuple(where(bnds)[0] + 1)  # ds starts at 1
@@ -125,7 +253,7 @@ def Dirichlet_bc_pml(Wave_object, W):
     return fix_bnd
 
 
-def construct_solver_or_matrix_with_pml_2d(Wave_object):
+def construct_solver_or_matrix_with_pml_2d(Wave_obj):
     '''
     Builds solver operators for 2D wave propagator with a PML. Doesn't create
     mass matrices if matrix_free option is on, which it is by default.
@@ -139,15 +267,15 @@ def construct_solver_or_matrix_with_pml_2d(Wave_object):
     -------
     None
     '''
-    dt = Wave_object.dt
-    c_sqr_inv = 1. / (Wave_object.c * Wave_object.c)
+    dt = Wave_obj.dt
+    c_sqr_inv = 1. / (Wave_obj.c * Wave_obj.c)
 
-    V = Wave_object.function_space
-    Z = Wave_object.vector_function_space
+    V = Wave_obj.function_space
+    Z = Wave_obj.vector_function_space
     W = V * Z
-    Wave_object.mixed_function_space = W
-    dx = fire.dx(scheme=Wave_object.quadrature_rule)
-    # ds = fire.ds(scheme=Wave_object.surface_quadrature_rule)
+    Wave_obj.mixed_function_space = W
+    dx = fire.dx(scheme=Wave_obj.quadrature_rule)
+    # ds = fire.ds(scheme=Wave_obj.surface_quadrature_rule)
 
     u, pp = fire.TrialFunctions(W)
     v, qq = fire.TestFunctions(W)
@@ -159,12 +287,13 @@ def construct_solver_or_matrix_with_pml_2d(Wave_object):
     u_n, pp_n = X_n.subfunctions
     u_nm1, _ = X_nm1.subfunctions
 
-    Wave_object.u_n = u_n
-    Wave_object.X = X
-    Wave_object.X_n = X_n
-    Wave_object.X_nm1 = X_nm1
+    Wave_obj.u_n = u_n
+    Wave_obj.X = X
+    Wave_obj.X_n = X_n
+    Wave_obj.X_nm1 = X_nm1
 
-    sigma_x, sigma_z = Wave_object.sigma_x, Wave_object.sigma_z
+    # sigma_x, sigma_z = Wave_obj.sigma_x, Wave_obj.sigma_z
+    sigma_x, sigma_z = pml_sigma_field(Wave_obj)
     Gamma_1, Gamma_2 = damping_pml_2d(sigma_z, sigma_x)
 
     B = fire.Cofunction(W.dual())
@@ -190,30 +319,30 @@ def construct_solver_or_matrix_with_pml_2d(Wave_object):
     rhs_ = fire.rhs(FF)
 
     # Apply Dirichlet BCs to the PML boundaries
-    fix_bnd = Dirichlet_bc_pml(Wave_object, W)
+    fix_bnd = Dirichlet_bc_pml(Wave_obj, W)
 
     A = fire.assemble(lhs_, mat_type="matfree", bcs=fix_bnd)
     solver = fire.LinearSolver(
-        A, solver_parameters=Wave_object.solver_parameters)
-    Wave_object.solver = solver
-    Wave_object.rhs = rhs_
-    Wave_object.B = B
+        A, solver_parameters=Wave_obj.solver_parameters)
+    Wave_obj.solver = solver
+    Wave_obj.rhs = rhs_
+    Wave_obj.B = B
 
 
-def construct_solver_or_matrix_with_pml_3d(Wave_object):
+def construct_solver_or_matrix_with_pml_3d(Wave_obj):
     """
     Builds solver operators for 3D wave propagator with a PML. Doesn't create
     mass matrices if matrix_free option is on, which it is by default.
     """
-    dt = Wave_object.dt
-    c_sqr_inv = 1. / (Wave_object.c * Wave_object.c)
+    dt = Wave_obj.dt
+    c_sqr_inv = 1. / (Wave_obj.c * Wave_obj.c)
 
-    V = Wave_object.function_space
-    Z = Wave_object.vector_function_space
+    V = Wave_obj.function_space
+    Z = Wave_obj.vector_function_space
     W = V * V * Z
-    Wave_object.mixed_function_space = W
-    dx = fire.dx(scheme=Wave_object.quadrature_rule)
-    # ds = fire.ds(scheme=Wave_object.surface_quadrature_rule)
+    Wave_obj.mixed_function_space = W
+    dx = fire.dx(scheme=Wave_obj.quadrature_rule)
+    # ds = fire.ds(scheme=Wave_obj.surface_quadrature_rule)
 
     u, psi, pp = fire.TrialFunctions(W)
     v, phi, qq = fire.TestFunctions(W)
@@ -225,13 +354,14 @@ def construct_solver_or_matrix_with_pml_3d(Wave_object):
     u_n, psi_n, pp_n = X_n.subfunctions
     u_nm1, psi_nm1, _ = X_nm1.subfunctions
 
-    Wave_object.u_n = u_n
-    Wave_object.X = X
-    Wave_object.X_n = X_n
-    Wave_object.X_nm1 = X_nm1
+    Wave_obj.u_n = u_n
+    Wave_obj.X = X
+    Wave_obj.X_n = X_n
+    Wave_obj.X_nm1 = X_nm1
 
-    sigma_x, sigma_z = Wave_object.sigma_x, Wave_object.sigma_z
-    sigma_y = Wave_object.sigma_y
+    # sigma_x, sigma_z = Wave_obj.sigma_x, Wave_obj.sigma_z
+    # sigma_y = Wave_obj.sigma_y
+    sigma_x, sigma_y, sigma_z = pml_sigma_field(Wave_obj)
     Gamma_1, Gamma_2, Gamma_3 = damping_pml_3d(sigma_z, sigma_x, sigma_y)
 
     B = fire.Cofunction(W.dual())
@@ -263,13 +393,13 @@ def construct_solver_or_matrix_with_pml_3d(Wave_object):
     rhs_ = fire.rhs(FF)
 
     # Apply Dirichlet BCs to the PML boundaries
-    fix_bnd = Dirichlet_bc_pml(Wave_object, W)
+    fix_bnd = Dirichlet_bc_pml(Wave_obj, W)
 
     A = fire.assemble(lhs_, mat_type="matfree", bcs=fix_bnd)
     solver = fire.LinearSolver(
-        A, solver_parameters=Wave_object.solver_parameters)
-    Wave_object.solver = solver
-    Wave_object.rhs = rhs_
-    Wave_object.B = B
+        A, solver_parameters=Wave_obj.solver_parameters)
+    Wave_obj.solver = solver
+    Wave_obj.rhs = rhs_
+    Wave_obj.B = B
 
     return
