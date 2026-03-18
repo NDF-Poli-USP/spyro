becf8a62b2d523f5910ab5a3befb336c508d3949
ruansava <ruansava@gmail.com>
2026-03-18 16:18:21 -0300

    Update __init__.py

--- spyro/utils/__init__.py
+++ spyro/utils/__init__.py
@@ -1,4 +1,4 @@
-from . import geometry_creation, estimate_timestep
+from . import geometry_creation
 from .utils import mpi_init, compute_functional, Mask, Gradient_mask_for_pml, run_in_one_core, write_hdf5_velocity_model
 from .analytical_solution_nodal import nodal_homogeneous_analytical
 from .velocity_to_grid import velocity_to_grid, change_scalar_field_resolution
