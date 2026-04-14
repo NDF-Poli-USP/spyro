84200bed136f83cbcbbb4581bcde755c8a133e92
ruansava <ruansava@gmail.com>
2026-04-14 19:57:51 -0300

    Update elastic_wave.py
    Added docstrings for class attributes and methods to improve documentation.

--- spyro/solvers/elastic_wave/elastic_wave.py
+++ spyro/solvers/elastic_wave/elastic_wave.py
@@ -11,14 +11,41 @@ from ...utils.typing import override
 
 
 class ElasticWave(Wave, metaclass=ABCMeta):
-    """Base class for elastic wave propagators."""
+    """Base class for elastic wave propagators.
+
+    Attributes
+    ----------
+    time : ufl.Constant
+        Time variable for the simulation, initialized to 0.
+    """
 
     def __init__(self, dictionary, comm=None):
+        """Initialize ElasticWave class.
+
+        Parameters
+        ----------
+        dictionary : dict
+            Configuration dictionary containing parameters for the elastic wave
+            propagation simulation.
+        comm : MPI.Comm, optional
+            MPI communicator
+        """
         super().__init__(dictionary, comm=comm)
         self.time = Constant(0)  # Time variable
 
     @override
     def _initialize_model_parameters(self):
+        """Initialize model parameters using synthetic data configuration.
+        
+        This method reads the 'synthetic_data' entry from the input dictionary
+        and initializes model parameters based on the specified data type.
+        
+        Raises
+        ------
+        Exception: 
+            If 'synthetic_data' key is missing, if 'type' is not present,
+            or if an invalid synthetic data type is specified.
+        """
         d = self.input_dictionary.get("synthetic_data", False)
         if bool(d) and "type" in d:
             if d["type"] == "object":
@@ -32,12 +59,12 @@ class ElasticWave(Wave, metaclass=ABCMeta):
 
     @abstractmethod
     def initialize_model_parameters_from_object(self, synthetic_data_dict):
-        """Initilize model parameters from object, based on this abstract method."""
+        """ Initialize model parameters from object, based on this abstract method."""
         pass
 
     @abstractmethod
     def initialize_model_parameters_from_file(self, synthetic_data_dict):
-        """Initilize model parameters from file, based on this abstract method."""
+        """ Initialize model parameters from file, based on this abstract method."""
         pass
 
     @override
