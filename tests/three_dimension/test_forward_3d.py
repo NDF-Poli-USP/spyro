59cb0dd1f3ef8ab5161cfcc2e174e59878b7df8f
Daiane Iglesia Dolci <63597005+Ig-dolci@users.noreply.github.com>
2026-05-18 10:53:47 -0300

    Apply suggestions from code review
    Co-authored-by: Daiane Iglesia Dolci <63597005+Ig-dolci@users.noreply.github.com>

--- tests/three_dimension/test_forward_3d.py
+++ tests/three_dimension/test_forward_3d.py
@@ -42,7 +42,7 @@ def test_forward_3d(tf=0.6):
         "length_z": 5.175,  # depth in km - always positive
         "length_x": 7.50,  # width in km - always positive
         "length_y": 7.50,  # thickness in km - always positive
-        "meshVTK": "meshes/overthrust_3D_true_model.msh",
+        "meshfile": "meshes/overthrust_3D_true_model.msh",
         "initmodel": "velocity_models/overthrust_3D_guess_model.hdf5",
         "truemodel": "velocity_models/overthrust_3D_true_model.hdf5",
     }
