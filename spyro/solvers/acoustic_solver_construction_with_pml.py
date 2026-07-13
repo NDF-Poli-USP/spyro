1286c35410cac94a98c9041c28453ecd6d14eb70
ruansava <ruansava@gmail.com>
2026-07-13 13:35:57 -0300

    Update acoustic_solver_construction_with_pml.py

--- spyro/solvers/acoustic_solver_construction_with_pml.py
+++ spyro/solvers/acoustic_solver_construction_with_pml.py
@@ -3,7 +3,7 @@
 from firedrake import (Cofunction, DirichletBC, div, dot, ds as fire_ds, dx as fire_dx,
                        Function, grad, inner, lhs, LinearVariationalProblem,
                        LinearVariationalSolver, rhs, split, TestFunctions, TrialFunctions)
-from ..utils.typing import BoundaryConditionsTypefrom, LayerDampingType
+from ..utils.typing import BoundaryConditionsType, LayerDampingType
 
 
 # Work from Keith Roberts, Eduardo Moscatelli,
