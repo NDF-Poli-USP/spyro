33c7ae04c677ee138d9a05386fdb66db4b737ee0
Filipe Tressmann <filipetressmann2@gmail.com>
2026-07-23 10:50:45 -0300

    Add basic models

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     url="https://github.com/NDF-Poli-USP/spyro",
     packages=find_packages(),
     install_requires=[
+        "dataclasses",
         "numpy",
         "scipy",
         "matplotlib",
