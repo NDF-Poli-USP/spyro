3488305154d92493fad3d352e0d6d8e94cfb2c84
Olender <olender@usp.br>
2026-07-28 18:45:58 +0100

    simple test

---
+++ tests/demo_tests/test_acoustic_marmousi_forward_propagation.py
@@ -0,0 +1,5 @@
+
+def test_acoustic_marmousi_forward():
+    from demos.acoustic_marmousi_forward_propagation import test_real_shot_record_generation_parallel
+    test_real_shot_record_generation_parallel()
+    assert True
