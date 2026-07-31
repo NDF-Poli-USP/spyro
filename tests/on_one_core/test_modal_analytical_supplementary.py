Fix Dirichlet hypershape frequency scaling
Fix Dirichlet hypershape frequency scaling

tests/on_one_core/test_modal_analytical_supplementary.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
+from firedrake import (Function, FunctionSpace, SpatialCoordinate,
+                       UnitCubeMesh, UnitSquareMesh)
 from scipy.special import jn_zeros
 from spyro.solvers.modal.modal_ana_sol import Modal_Analytical_Solver
+from spyro.utils.error_management import type_firedrake_error
 
 
 class TestModalAnalyticalSolver:
@@ -16,6 +19,26 @@ class TestModalAnalyticalSolver:
     def solver_3d(self):
         """Create a 3D solver instance."""
         return Modal_Analytical_Solver(dimension=3)
+
+    @pytest.fixture
+    def mesh_2d(self):
+        """Create a 2D mesh."""
+        return UnitSquareMesh(4, 4)
+
+    @pytest.fixture
+    def mesh_3d(self):
+        """Create a 3D mesh."""
+        return UnitCubeMesh(4, 4, 4)
+
+    @pytest.fixture
+    def V_2d(self, mesh_2d):
+        """Create a 2D function space."""
+        return FunctionSpace(mesh_2d, "KMV", 4)
+
+    @pytest.fixture
+    def V_3d(self, mesh_3d):
+        """Create a 3D function space."""
+        return FunctionSpace(mesh_3d, "KMV", 3)
 
     def test_freq_factor_rec_dirichlet_2d(self, solver_2d):
         """Test _freq_factor_rec with Dirichlet BC for 2D rectangle."""
@@ -308,8 +331,8 @@ class TestModalAnalyticalSolver:
             pn, qn, fr_ell, fr_rec = solver_2d._reg_geometry_hyp(cut_plane_percent=cut)
 
             # Check fitted parameters are valid
-            assert np.isfinite(pn) and pn > 0
-            assert np.isfinite(qn) and qn > 0
+            assert np.isfinite(pn) and pn > 0.
+            assert np.isfinite(qn) and qn > 0.
 
             # As cut_percent increases (less truncation), fr_ell and fr_rec should increase
             if prev_fr_ell is not None:
@@ -328,8 +351,8 @@ class TestModalAnalyticalSolver:
             pn, qn, fr_ell, fr_rec = solver_3d._reg_geometry_hyp(cut_plane_percent=cut)
 
             # Check fitted parameters are valid
-            assert np.isfinite(pn) and pn > 0
-            assert np.isfinite(qn) and qn > 0
+            assert np.isfinite(pn) and pn > 0.
+            assert np.isfinite(qn) and qn > 0.
 
             # As cut_percent increases (less truncation), fr_ell and fr_rec should increase
             if prev_fr_ell is not None:
@@ -345,62 +368,262 @@ class TestModalAnalyticalSolver:
         """Test _freq_factor_hyp with Neumann BC for 2D."""
         # Test parameters
         hyp_degrees = [3, 5, 10, 20, 50, 100, 200, 1000]
-        f_rec = 1.0
-        f_ell = 2.0
+        hyper_axes = (2., 3.)
         c_eq = 3.0
 
         for n_hyp in hyp_degrees:
 
-            f_hyp, c_reg = solver_2d._freq_factor_hyp(n_hyp, f_rec, f_ell,
-                                                      c_eq, bc="Neumann")
+            # Fundamental frequency factors
+            f_ell_neu = solver_2d._freq_factor_ell(hyper_axes, bc="Neumann")
+            f_rec_neu = solver_2d._freq_factor_rec(hyper_axes, bc="Neumann")
+            f_ell_dir = solver_2d._freq_factor_ell(hyper_axes, bc="Dirichlet")
+            f_rec_dir = solver_2d._freq_factor_rec(hyper_axes, bc="Dirichlet")
+            fbc_dirichlet = (f_ell_dir / f_ell_neu, f_rec_dir / f_rec_neu)
+
+            f_hyp_dir, c_reg_dir = solver_2d._freq_factor_hyp(n_hyp, f_rec_dir, f_ell_dir,
+                                                              c_eq, bc="Dirichlet",
+                                                              fbc_dirichlet=fbc_dirichlet)
 
             # Check results are positive and finite
-            assert np.isfinite(f_hyp) and f_hyp > 0
-            assert np.isfinite(c_reg) and c_reg > 0
+            assert np.isfinite(f_hyp_dir) and f_hyp_dir > 0.
+            assert np.isfinite(c_reg_dir) and c_reg_dir > 0.
 
-            # f_hyp should be between f_ell and f_rec
-            assert f_rec < f_hyp < f_ell
+            # f_hyp_dir should be between f_rec_dir and f_ell_dir
+            assert f_rec_dir < f_hyp_dir < f_ell_dir
 
-        for n_hyp in hyp_degrees:
-
-            f_hyp, c_reg = solver_2d._freq_factor_hyp(n_hyp, f_rec, f_ell,
-                                                      c_eq, bc="Dirichlet")
+            f_hyp_neu, c_reg_neu = solver_2d._freq_factor_hyp(n_hyp, f_rec_neu, f_ell_neu,
+                                                              c_eq, bc="Neumann")
 
             # Check results are positive and finite
-            assert np.isfinite(f_hyp) and f_hyp > 0
-            assert np.isfinite(c_reg) and c_reg > 0
+            assert np.isfinite(f_hyp_neu) and f_hyp_neu > 0.
+            assert np.isfinite(c_reg_neu) and c_reg_neu > 0.
 
-            # f_hyp should be between f_ell and f_rec
-            assert f_rec < f_hyp < f_ell
+            # f_hyp_neu should be between f_rec_neu and f_ell_neu
+            assert f_rec_neu < f_hyp_neu < f_ell_neu
+
+            # f_hyp_dir should greater than f_hyp_neu for the same n_hyp and c_eq
+            assert f_rec_neu < f_hyp_neu < f_hyp_dir < f_ell_dir
 
     def test_freq_factor_hyp_3d(self, solver_3d):
         """Test _freq_factor_hyp with Neumann BC fpr 3D."""
         # Test parameters
         hyp_degrees = [3, 5, 10, 20, 50, 100, 200, 1000]
-        f_rec = 1.0
-        f_ell = 2.0
+        hyper_axes = (2., 3., 1.)
         c_eq = 3.0
-
         for n_hyp in hyp_degrees:
 
-            f_hyp, c_reg = solver_3d._freq_factor_hyp(n_hyp, f_rec, f_ell,
-                                                      c_eq, bc="Neumann")
+            # Fundamental frequency factors
+            f_ell_neu = solver_3d._freq_factor_ell(hyper_axes, bc="Neumann")
+            f_rec_neu = solver_3d._freq_factor_rec(hyper_axes, bc="Neumann")
+            f_ell_dir = solver_3d._freq_factor_ell(hyper_axes, bc="Dirichlet")
+            f_rec_dir = solver_3d._freq_factor_rec(hyper_axes, bc="Dirichlet")
+            fbc_dirichlet = (f_ell_dir / f_ell_neu, f_rec_dir / f_rec_neu)
+
+            f_hyp_dir, c_reg_dir = solver_3d._freq_factor_hyp(n_hyp, f_rec_dir, f_ell_dir,
+                                                              c_eq, bc="Dirichlet",
+                                                              fbc_dirichlet=fbc_dirichlet)
 
             # Check results are positive and finite
-            assert np.isfinite(f_hyp) and f_hyp > 0
-            assert np.isfinite(c_reg) and c_reg > 0
+            assert np.isfinite(f_hyp_dir) and f_hyp_dir > 0.
+            assert np.isfinite(c_reg_dir) and c_reg_dir > 0.
 
-            # f_hyp should be between f_ell and f_rec
-            assert f_rec < f_hyp < f_ell
+            # f_hyp_dir should be between f_rec_dir and f_ell_dir
+            assert f_rec_dir < f_hyp_dir < f_ell_dir
 
-        for n_hyp in hyp_degrees:
-
-            f_hyp, c_reg = solver_3d._freq_factor_hyp(n_hyp, f_rec, f_ell,
-                                                      c_eq, bc="Dirichlet")
+            f_hyp_neu, c_reg_neu = solver_3d._freq_factor_hyp(n_hyp, f_rec_neu, f_ell_neu,
+                                                              c_eq, bc="Neumann")
 
             # Check results are positive and finite
-            assert np.isfinite(f_hyp) and f_hyp > 0
-            assert np.isfinite(c_reg) and c_reg > 0
+            assert np.isfinite(f_hyp_neu) and f_hyp_neu > 0.
+            assert np.isfinite(c_reg_neu) and c_reg_neu > 0.
 
-            # f_hyp should be between f_ell and f_rec
-            assert f_rec < f_hyp < f_ell
+            # f_hyp_neu should be between f_rec_neu and f_ell_neu
+            assert f_rec_neu < f_hyp_neu < f_ell_neu
+
+            # f_hyp_dir should greater than f_hyp_neu for the same n_hyp and c_eq
+            assert f_rec_neu < f_hyp_neu < f_hyp_dir < f_ell_dir
+
+    def test_dummy_load_static(self, solver_2d, V_2d, solver_3d, V_3d):
+        """Test basic functionality of dummy_load_static."""
+        dof_load = np.array([0, 5, 10])
+        amplitude_load = np.array([1., -2., 3.])
+
+        for dimension in [2, 3]:
+            if dimension == 2:
+                solver = solver_2d
+                V = V_2d
+            else:
+                solver = solver_3d
+                V = V_3d
+
+            q_dummy, q_ref = solver.dummy_load_static(V, dof_load, amplitude_load)
+
+            # Check that q_dummy is a Function
+            assert type_firedrake_error("q_dummy", q_dummy, "Function")
+            assert q_dummy.function_space() == V
+
+            # Check values at specified DOFs
+            assert np.all(q_dummy.dat.data[dof_load] == amplitude_load)
+
+            # Check that other DOFs remain zero
+            other_dofs = np.setdiff1d(np.arange(V.dim()), dof_load)
+            assert sum(q_dummy.dat.data[other_dofs]) == 0.
+
+            # q_ref should be None when V_ref is not provided
+            assert q_ref is None
+
+    def test_dummy_load_static_with_reference(self, solver_2d, V_2d, solver_3d, V_3d):
+        """Test dummy_load_static with reference function space."""
+        dof_load = np.array([0, 5, 10])
+        amplitude_load = np.array([1., -2., 3.])
+
+        for dimension in [2, 3]:
+            if dimension == 2:
+                solver = solver_2d
+                V = V_2d
+                mesh = V_2d.mesh()
+
+            else:
+                solver = solver_3d
+                V = V_3d
+                mesh = V_3d.mesh()
+
+            # Create a different function space for reference
+            V_ref = FunctionSpace(mesh, "CG", 3)
+
+            q_dummy, q_ref = solver_2d.dummy_load_static(V, dof_load,
+                                                         amplitude_load,
+                                                         V_ref=V_ref)
+
+            # q_dummy should be in V
+            assert type_firedrake_error("q_dummy", q_dummy, "Function")
+            assert q_dummy.function_space() == V
+
+            # Check values at specified DOFs
+            assert np.all(q_dummy.dat.data[dof_load] == amplitude_load)
+
+            # Check that other DOFs remain zero
+            other_dofs = np.setdiff1d(np.arange(V.dim()), dof_load)
+            assert sum(q_dummy.dat.data[other_dofs]) == 0.
+
+            # q_ref should be in V_ref
+            assert type_firedrake_error("q_ref", q_ref, "Function")
+            assert q_ref.function_space() == V_ref
+
+    def test_c_equivalent_volume_homog_constant_2d(self, solver_2d, V_2d):
+        """Test c_equivalent with volume homogenization for constant velocity in 2D."""
+        # Create constant velocity
+        c = Function(V_2d)
+        c.assign(3.0)  # Constant velocity of 3.0
+
+        # Compute equivalent velocity
+        c_eq = solver_2d.c_equivalent(c, V_2d, type_homog="volume")
+
+        # For constant velocity, c_eq should equal the constant value
+        np.testing.assert_almost_equal(c_eq, 3.0, decimal=10)
+
+    def test_c_equivalent_volume_homog_constant_3d(self, solver_3d, V_3d):
+        """Test c_equivalent with volume homogenization for constant velocity in 3D."""
+        # Create constant velocity
+        c = Function(V_3d)
+        c.assign(2.5)
+
+        # Compute equivalent velocity
+        c_eq = solver_3d.c_equivalent(c, V_3d, type_homog="volume")
+
+        # For constant velocity, c_eq should equal the constant value
+        np.testing.assert_almost_equal(c_eq, 2.5, decimal=10)
+
+    def test_c_equivalent_volume_homog_variable_2d(self, solver_2d, V_2d):
+        """Test c_equivalent with volume homogenization for variable velocity in 2D."""
+        # Create variable velocity: c(x,y) = 1 + x + y
+        x = SpatialCoordinate(V_2d.mesh())
+        c = Function(V_2d)
+        c.interpolate(1.0 + x[0] + x[1])
+
+        # Compute equivalent velocity
+        c_eq = solver_2d.c_equivalent(c, V_2d, type_homog="volume")
+
+        # For volume homogenization, c_eq should be the average velocity
+        # Expected average of (1 + x + y) over unit square = 1 + 0.5 + 0.5 = 2.0
+        np.testing.assert_almost_equal(c_eq, 2.0, decimal=6)
+
+    def test_c_equivalent_volume_homog_variable_3d(self, solver_3d, V_3d):
+        """Test c_equivalent with volume homogenization for variable velocity in 3D."""
+        # Create variable velocity: c(x,y,z) = 2 + x + y + z
+        x = SpatialCoordinate(V_3d.mesh())
+        c = Function(V_3d)
+        c.interpolate(2.0 + x[0] + x[1] + x[2])
+
+        # Compute equivalent velocity
+        c_eq = solver_3d.c_equivalent(c, V_3d, type_homog="volume")
+
+        # Expected average of (2 + x + y + z) over unit cube = 2 + 0.5 + 0.5 + 0.5 = 3.5
+        np.testing.assert_almost_equal(c_eq, 3.5, decimal=6)
+
+    def test_c_equivalent_energy_homog_constant_2d(self, solver_2d, V_2d):
+        """Test c_equivalent with energy homogenization for constant velocity in 2D."""
+        # Create constant velocity
+        c = Function(V_2d).assign(3.)
+
+        # Create a static load
+        dof_load = np.array([0])
+        amplitude_load = np.array([1.])
+        q_dummy = solver_2d.dummy_load_static(V_2d, dof_load, amplitude_load)[0]
+
+        # Define quadrature rule
+        quad_rule = {"degree": 0}
+
+        # Compute equivalent velocity
+        c_eq = solver_2d.c_equivalent(c, V_2d, type_homog="energy",
+                                      static_load_for_ceq=q_dummy,
+                                      quad_rule=quad_rule)
+
+        # For constant velocity, c_eq should be equal to the constant value
+        np.testing.assert_almost_equal(c_eq, 3., decimal=6)
+
+        # Should work with default load (constant load over domain)
+        c_eq = solver_2d.c_equivalent(c, V_2d, type_homog="energy",
+                                      static_load_for_ceq=None,
+                                      quad_rule=quad_rule)
+
+        # Should still give correct result
+        np.testing.assert_almost_equal(c_eq, 3., decimal=6)
+
+    def test_c_equivalent_energy_homog_constant_3d(self, solver_3d, V_3d):
+        """Test c_equivalent with energy homogenization for constant velocity in 3D."""
+        # Create constant velocity
+        c = Function(V_3d).assign(3.)
+
+        # Create a static load (point load at center)
+        dof_load = np.array([0])
+        amplitude_load = np.array([1.])
+        q_dummy = solver_3d.dummy_load_static(V_3d, dof_load, amplitude_load)[0]
+
+        # Define quadrature rule
+        quad_rule = {"degree": 0}
+
+        # Compute equivalent velocity
+        c_eq = solver_3d.c_equivalent(c, V_3d, type_homog="energy",
+                                      static_load_for_ceq=q_dummy,
+                                      quad_rule=quad_rule)
+
+        # For constant velocity, c_eq should be equal to the constant value
+        np.testing.assert_almost_equal(c_eq, 3., decimal=6)
+
+        # Should work with default load (constant load over domain)
+        c_eq = solver_3d.c_equivalent(c, V_3d, type_homog="energy",
+                                      static_load_for_ceq=None,
+                                      quad_rule=quad_rule)
+
+        # Should still give correct result
+        np.testing.assert_almost_equal(c_eq, 3., decimal=6)
+
+    def test_c_equivalent_invalid_type_homog(self, solver_2d, V_2d):
+        """Test c_equivalent with invalid type_homog."""
+        c = Function(V_2d)
+        c.assign(3.0)
+
+        # Should raise ValueError for invalid type_homog
+        with pytest.raises(ValueError):
+            solver_2d.c_equivalent(c, V_2d, type_homog="invalid")