91e8997ff5dd35dc78121684c23a8bcb96efe952
Olender <olender@usp.br>
2026-08-05 22:31:29 +0100

    adding pyproject toml

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="spyro",
-    version="0.9.1",
-    license="LGPL v3",
-    description="Wave modeling with the finite element method",
-    author="Keith J. Roberts, Alexandre F. G. Olender, Ruben Andres Salas, Eduardo Moscatelli de Souza, Daiane I. Dolci, Thiago Dias dos Santos, Lucas Franceschini",
-    url="https://github.com/NDF-Poli-USP/spyro",
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "segyio",
-        "meshio",
-        "mpi-pytest"],
-)
+
+setup()
