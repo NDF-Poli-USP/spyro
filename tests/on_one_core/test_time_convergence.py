9f04141acc6689379d79372896865b5812c23710
ruansava <ruansava@gmail.com>
2026-08-18 11:30:03 -0300

    Update tests/on_one_core/test_time_convergence.py
    Co-authored-by: Alexandre Olender <45005909+Olender@users.noreply.github.com>

--- tests/on_one_core/test_time_convergence.py
+++ tests/on_one_core/test_time_convergence.py
@@ -87,8 +87,8 @@ def run_forward(dt, with_pml=False):
     return rec_out
 
 
-@mark.slow
-@mark.parametrize("with_pml", [False, True])
+@pytest.mark.slow
+@pytest.mark.parametrize("with_pml", [False, True])
 def test_second_order_time_convergence(with_pml):
     """Test that the second order time convergence
     of the central difference method is achieved"""
