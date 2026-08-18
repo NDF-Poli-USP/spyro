6f2396694644331013465f59526b8a184276a83c
ruansava <ruansava@gmail.com>
2026-08-18 11:28:46 -0300

    Update tests/on_one_core/test_analytical_solution.py
    Co-authored-by: Alexandre Olender <45005909+Olender@users.noreply.github.com>

--- tests/on_one_core/test_analytical_solution.py
+++ tests/on_one_core/test_analytical_solution.py
@@ -5,7 +5,7 @@ from spyro.io.basicio import parallel_print as pprint
 from spyro.tools.error_measure import MeasureError
 
 
-@mark.parametrize("use_vertex_only_mesh", [False, True])
+@pytest.mark.parametrize("use_vertex_only_mesh", [False, True])
 def test_analytical_solution(use_vertex_only_mesh):
     frequency = 5.0
     offset = 0.5
