7c7d9003fab595c250a883aa2ddfa9fefe23a400
Iglesia Dolci <daia.dolci@gmail.com>
2026-09-21 11:37:22 -0300

    update plots

--- spyro/plots/__init__.py
+++ spyro/plots/__init__.py
@@ -1,8 +1,6 @@
 """Plotting helpers for wave simulation outputs."""
 
-from .general_plots import (
-    plot_shots, plot_function, plot_model, plot_model_in_p1,
-)
+from .general_plots import plot_shots, plot_function, plot_model, plot_model_in_p1
 from .mesh_plots import plot_mesh_sizes
 from .debug_plots import debug_plot, debug_pvd
 from .receiver_plots import plot_receiver_response, plot_displacement_components
